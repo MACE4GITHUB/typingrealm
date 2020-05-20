2ed9b85b7c55dcd8833154313a58f10fdb1e6435
Ivan Zyranau <ewancoder@gmail.com>
2020-05-21 01:00:57 +0300

    Add Announce message for broadcast announcement
    This is the message that will be later used for server-side
    announcement e.g. about server shut-down.

---
+++ TypingRealm.Messaging.Core/Messages/Announce.cs
@@ -0,0 +1,16 @@
+﻿namespace TypingRealm.Messaging.Messages
+{
+    /// <summary>
+    /// Broadcast announcement to all the clients.
+    /// </summary>
+    [Message]
+    public sealed class Announce
+    {
+#pragma warning disable CS8618
+        public Announce() { }
+#pragma warning restore CS8618
+        public Announce(string message) => Message = message;
+
+        public string Message { get; set; }
+    }
+}
