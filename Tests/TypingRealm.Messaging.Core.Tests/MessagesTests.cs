29764a7270e35d77eb63558395709efcc90d4f99
Ivan Zyranau <ewancoder@gmail.com>
2020-05-21 01:18:38 +0300

    Add unit tests for Announce message
    Also add simple testing base class with AutoFixture's Fixture and
    AssertSerializable method to check if the message is serializable.

---
+++ Tests/TypingRealm.Messaging.Core.Tests/MessagesTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Reflection;
+using TypingRealm.Messaging.Messages;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Tests
+{
+    public class MessagesTests : TestsBase
+    {
+        [Fact]
+        public void ShouldBe1MessageInAssembly()
+        {
+            Assert.Equal(1, typeof(Announce).Assembly.GetTypes().Count(
+                t => t.GetCustomAttribute<MessageAttribute>() != null));
+        }
+
+        [Fact]
+        public void AnnounceMessage()
+        {
+            AssertSerializable<Announce>();
+
+            var sut = new Announce
+            {
+                Message = "message"
+            };
+            Assert.Equal("message", sut.Message);
+
+            sut = new Announce("message");
+            Assert.Equal("message", sut.Message);
+        }
+    }
+}
---
+++ TypingRealm.Testing/TestsBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using AutoFixture;
+using Xunit;
+
+namespace TypingRealm.Testing
+{
+    public abstract class TestsBase
+    {
+        private readonly Fixture _fixture = new Fixture();
+
+        protected T Create<T>()
+        {
+            return _fixture.Create<T>();
+        }
+
+        protected void AssertSerializable<T>()
+        {
+            var message = Create<T>();
+            var result = JsonSerializer.Deserialize<T>(
+                JsonSerializer.Serialize(message));
+
+            foreach (var property in typeof(T).GetProperties())
+            {
+                Assert.Equal(property.GetValue(message), property.GetValue(result));
+            }
+        }
+    }
+}
