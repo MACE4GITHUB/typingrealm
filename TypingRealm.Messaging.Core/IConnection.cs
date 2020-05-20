6f4b38eecf0cf3ec6ebef22cb5c2f2984d9be1a9
Ivan Zyranau <ewancoder@gmail.com>
2020-05-21 00:51:29 +0300

    Add IConnection and MessageAttribute abstractions
    IConnection is an abstraction over the actual client-to-server
    connection capable of sending messages over the wire, MessageAttribute
    is for marking serializable messages that can be sent and handled.

---
+++ TypingRealm.Messaging.Core/IConnection.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TypingRealm.Messaging
+{
+    /// <summary>
+    /// Connection between server and client capable of sending and receiving
+    /// messages marked with <see cref="MessageAttribute"/> attribute.
+    /// </summary>
+    public interface IConnection : IMessageSender, IMessageReceiver
+    {
+    }
+
+    /// <summary>
+    /// Connection between server and client capable of sending messages marked
+    /// with <see cref="MessageAttribute"/> attribute.
+    /// </summary>
+    public interface IMessageSender
+    {
+        ValueTask SendAsync(object message, CancellationToken cancellationToken);
+    }
+
+    /// <summary>
+    /// Connection between server and client capable of receiving messages
+    /// marked with <see cref="MessageAttribute"/> attribute.
+    /// </summary>
+    public interface IMessageReceiver
+    {
+        ValueTask<object> ReceiveAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ TypingRealm.Messaging.Core/MessageAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TypingRealm.Messaging
+{
+    /// <summary>
+    /// Indicates that type is serializable message that can be sent over
+    /// <see cref="IConnection"/> implementation.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class MessageAttribute : Attribute
+    {
+    }
+}
