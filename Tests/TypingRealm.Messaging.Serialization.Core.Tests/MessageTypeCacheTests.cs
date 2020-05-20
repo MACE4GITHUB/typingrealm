6a0ad8d7fa875cbc4f254c39806e1d734e289df7
Ivan Zyranau <ewancoder@gmail.com>
2020-05-21 02:21:47 +0300

    Add MessageCacheType implementation
    The implementation sorts types by FullName and indexes them starting
    from 1, using these indexes as message type identifiers.

---
+++ Tests/TypingRealm.Messaging.Serialization.Core.Tests/MessageTypeCacheTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace TypingRealm.Messaging.Serialization.Tests
+{
+    namespace A
+    {
+        [Message]
+        public class ATestMessage
+        {
+        }
+
+        [Message]
+        public class BTestMessage
+        {
+        }
+    }
+
+    namespace B
+    {
+        [Message]
+        public class ATestMessage
+        {
+        }
+
+        [Message]
+        public class BTestMessage
+        {
+        }
+    }
+
+    public class MessageTypeCacheTests
+    {
+        [Fact]
+        public void ShouldThrowIfSuppliedTypesContainDuplicates()
+        {
+            Assert.Throws<ArgumentException>(
+                () => new MessageTypeCache(new[]
+                {
+                    typeof(A.ATestMessage),
+                    typeof(A.ATestMessage)
+                }));
+        }
+
+        [Fact]
+        public void ShouldBeEmptyWhenNoMessages()
+        {
+            var sut = new MessageTypeCache(Enumerable.Empty<Type>());
+            Assert.Empty(sut.GetAllTypes());
+        }
+
+        [Fact]
+        public void ShouldSortByTypeFullNameAndAssignNumberStartingFrom1()
+        {
+            var sut = new MessageTypeCache(new[]
+            {
+                typeof(A.BTestMessage),
+                typeof(B.BTestMessage),
+                typeof(B.ATestMessage),
+                typeof(A.ATestMessage)
+            });
+
+            var all = sut.GetAllTypes().ToDictionary(x => x.Key, x => x.Value);
+            Assert.Equal(4, all.Count);
+            Assert.Equal(typeof(A.ATestMessage), all["1"]);
+            Assert.Equal(typeof(A.BTestMessage), all["2"]);
+            Assert.Equal(typeof(B.ATestMessage), all["3"]);
+            Assert.Equal(typeof(B.BTestMessage), all["4"]);
+        }
+
+        [Fact]
+        public void ShouldGetTypeById()
+        {
+            var sut = new MessageTypeCache(new[] { typeof(A.ATestMessage) });
+            Assert.Equal(typeof(A.ATestMessage), sut.GetTypeById("1"));
+        }
+
+        [Fact]
+        public void ShouldGetIdByType()
+        {
+            var sut = new MessageTypeCache(new[] { typeof(A.ATestMessage) });
+            Assert.Equal("1", sut.GetTypeId(typeof(A.ATestMessage)));
+        }
+
+        [Fact]
+        public void GetTypeById_ShouldThrowWhenTypeIsNotInCache()
+        {
+            var sut = new MessageTypeCache(new[] { typeof(A.ATestMessage) });
+            Assert.Throws<InvalidOperationException>(
+                () => sut.GetTypeById("2"));
+        }
+
+        [Fact]
+        public void GetTypeId_ShouldThrowWhenTypeIsNotInCache()
+        {
+            var sut = new MessageTypeCache(new[] { typeof(A.ATestMessage) });
+            Assert.Throws<InvalidOperationException>(
+                () => sut.GetTypeId(typeof(B.BTestMessage)));
+        }
+    }
+}
---
+++ TypingRealm.Messaging.Serialization.Core/MessageTypeCache.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TypingRealm.Messaging.Serialization
+{
+    /// <summary>
+    /// The cache stores mapping between message type and its string identifier.
+    /// </summary>
+    public sealed class MessageTypeCache : IMessageTypeCache
+    {
+        private readonly Dictionary<string, Type> _idToType;
+        private readonly Dictionary<Type, string> _typeToId;
+
+        public MessageTypeCache(IEnumerable<Type> messageTypes)
+        {
+            _idToType = ToDictionaryById(messageTypes);
+            _typeToId = _idToType.ToDictionary(x => x.Value, x => x.Key);
+        }
+
+        public IEnumerable<KeyValuePair<string, Type>> GetAllTypes()
+        {
+            return _idToType;
+        }
+
+        public Type GetTypeById(string typeId)
+        {
+            if (!_idToType.ContainsKey(typeId))
+                throw new InvalidOperationException($"Type with id {typeId} doesn't exist in {nameof(MessageTypeCache)}.");
+
+            return _idToType[typeId];
+        }
+
+        public string GetTypeId(Type type)
+        {
+            if (!_typeToId.ContainsKey(type))
+                throw new InvalidOperationException($"Type {type.FullName} doesn't exist in {nameof(MessageTypeCache)}.");
+
+            return _typeToId[type];
+        }
+
+        private static Dictionary<string, Type> ToDictionaryById(IEnumerable<Type> messageTypes)
+        {
+            return messageTypes
+                .OrderBy(type => type.FullName)
+                .Select((type, index) => new
+                {
+                    TypeId = (index + 1).ToString(),
+                    Type = type
+                })
+                .ToDictionary(x => x.TypeId, x => x.Type);
+        }
+    }
+}
