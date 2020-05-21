f77a7a29aa83a373f5428912d66e3ccc97aec357
Ivan Zyranau <ewancoder@gmail.com>
2020-05-21 16:52:15 +0300

    Add IConnectionHandler interface
    The implementation will handle the connection using Messaging's and
    Domain's game business logic.

---
+++ TypingRealm.Messaging/IConnectionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TypingRealm.Messaging
+{
+    /// <summary>
+    /// Handles incoming connection.
+    /// </summary>
+    public interface IConnectionHandler
+    {
+        /// <summary>
+        /// Handles connection. This method may never end. It will continue
+        /// executing as long as the connection lives. This method is the entry
+        /// point of the main logic of the messaging framework that allows
+        /// handling and sending of messages. It should be called after
+        /// connection is established and it should be canceled by cancellation
+        /// token when the server is going to shut down.
+        /// </summary>
+        /// <param name="connection">Connection to the client.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        Task HandleAsync(IConnection connection, CancellationToken cancellationToken);
+    }
+}
