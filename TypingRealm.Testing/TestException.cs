81ee9b0479b2f8a0af5ff15ec5a2e64b26f2d9b0
Ivan Zyranau <ewancoder@gmail.com>
2020-05-21 18:19:34 +0300

    Add documentation comment for TestException

--- TypingRealm.Testing/TestException.cs
+++ TypingRealm.Testing/TestException.cs
@@ -2,6 +2,9 @@
 
 namespace TypingRealm.Testing
 {
+    /// <summary>
+    /// Throw this exception when you need to test custom exception handling.
+    /// </summary>
     public class TestException : Exception
     {
         public TestException() { }
