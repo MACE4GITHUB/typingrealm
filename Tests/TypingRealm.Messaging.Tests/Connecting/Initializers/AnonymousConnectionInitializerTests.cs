7f1e3701098db9eb306d918ea69a0c4278a6e106
Ivan Zyranau <ewancoder@gmail.com>
2020-05-22 01:15:36 +0300

    Add AnonymousConnectionInitializer
    It will allow all incoming connections and will start handling messages
    straightaway.

---
+++ Tests/TypingRealm.Messaging.Tests/Connecting/Initializers/AnonymousConnectionInitializerTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using TypingRealm.Messaging.Connecting.Initializers;
+using TypingRealm.Messaging.Updating;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Tests.Connecting.Initializers
+{
+    public class AnonymousConnectionInitializerTests : TestsBase
+    {
+        [Theory, AutoMoqData]
+        public async Task ShouldCreateUniqueGuidIdentities(AnonymousConnectionInitializer sut)
+        {
+            var client1 = await sut.ConnectAsync(Create<IConnection>(), default);
+            Assert.False(string.IsNullOrWhiteSpace(client1.ClientId));
+            Assert.NotEqual(Guid.Empty, Guid.Parse(client1.ClientId));
+
+            var client2 = await sut.ConnectAsync(Create<IConnection>(), default);
+            Assert.False(string.IsNullOrWhiteSpace(client2.ClientId));
+            Assert.NotEqual(Guid.Empty, Guid.Parse(client2.ClientId));
+
+            Assert.NotEqual(client1.ClientId, client2.ClientId);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task ShouldSetConnection(
+            IConnection connection,
+            AnonymousConnectionInitializer sut)
+        {
+            var client = await sut.ConnectAsync(connection, default);
+            Assert.Equal(connection, client.Connection);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task ShouldSetUpdateDetector(
+            [Frozen]IUpdateDetector updateDetector,
+            AnonymousConnectionInitializer sut)
+        {
+            var client = await sut.ConnectAsync(Create<IConnection>(), default);
+            Assert.Equal(updateDetector, GetPrivateField(client, "_updateDetector"));
+        }
+
+        [Theory, AutoMoqData]
+        public async Task ShouldSetGroupToLobbyByDefault(AnonymousConnectionInitializer sut)
+        {
+            var client = await sut.ConnectAsync(Create<IConnection>(), default);
+            Assert.Equal("Lobby", client.Group);
+        }
+    }
+}
---
+++ TypingRealm.Messaging/Connecting/Initializers/AnonymousConnectionInitializer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TypingRealm.Messaging.Updating;
+
+namespace TypingRealm.Messaging.Connecting.Initializers
+{
+    /// <summary>
+    /// Connection initializer that accepts all connections and assigns newly
+    /// generated GUID identity to every connected client and default "Lobby" group.
+    /// </summary>
+    public sealed class AnonymousConnectionInitializer : IConnectionInitializer
+    {
+        private const string DefaultGroup = "Lobby";
+        private readonly IUpdateDetector _updateDetector;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AnonymousConnectionInitializer"/> class.
+        /// </summary>
+        /// <param name="updateDetector">Update detector used when creating new
+        /// instances of <see cref="ConnectedClient"/>.</param>
+        public AnonymousConnectionInitializer(IUpdateDetector updateDetector)
+        {
+            _updateDetector = updateDetector;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="ConnectedClient"/> instance with new GUID
+        /// identity and default "Lobby" group.
+        /// </summary>
+        /// <param name="connection">Client's connection.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        public ValueTask<ConnectedClient> ConnectAsync(IConnection connection, CancellationToken cancellationToken)
+        {
+            var clientId = Guid.NewGuid().ToString();
+            var connectedClient = new ConnectedClient(clientId, connection, DefaultGroup, _updateDetector);
+
+            return new ValueTask<ConnectedClient>(connectedClient);
+        }
+    }
+}
