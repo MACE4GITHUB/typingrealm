1e1a57d0a278a0f36950b755122f7b9ee4a97738
Ivan Zyranau <ewancoder@gmail.com>
2020-05-22 02:26:54 +0300

    Add MessageDispatcher implementation

---
+++ Tests/TypingRealm.Messaging.Tests/Handling/MessageDispatcherTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using AutoFixture.Xunit2;
+using Moq;
+using TypingRealm.Messaging.Handling;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Tests.Handling
+{
+    public class AnotherTestMessage : TestMessage { }
+
+    public class MessageDispatcherTests : TestsBase
+    {
+        [Theory, AutoMoqData]
+        public async Task ShouldDispatchToMultipleHandlers(
+            ConnectedClient sender,
+            [Frozen]Mock<IMessageHandlerFactory> handlerFactory,
+            MessageDispatcher sut)
+        {
+            var handlers = Fixture.CreateMany<Mock<IMessageHandler<AnotherTestMessage>>>();
+            handlerFactory.Setup(x => x.GetHandlersFor<AnotherTestMessage>())
+                .Returns(handlers.Select(h => h.Object));
+            TestMessage message = new AnotherTestMessage();
+
+            await sut.DispatchAsync(sender, message, default);
+
+            foreach (var handler in handlers)
+            {
+                handler.Verify(x => x.HandleAsync(sender, (AnotherTestMessage)message, default));
+            }
+        }
+    }
+}
---
+++ TypingRealm.Messaging/Handling/MessageDispatcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TypingRealm.Messaging.Handling
+{
+    public sealed class MessageDispatcher : IMessageDispatcher
+    {
+        private readonly IMessageHandlerFactory _messageHandlerFactory;
+
+        public MessageDispatcher(IMessageHandlerFactory messageHandlerFactory)
+        {
+            _messageHandlerFactory = messageHandlerFactory;
+        }
+
+        public ValueTask DispatchAsync(ConnectedClient sender, object message, CancellationToken cancellationToken)
+        {
+            return DynamicDispatchAsync(sender, (dynamic)message, cancellationToken);
+        }
+
+        private ValueTask DynamicDispatchAsync<TMessage>(ConnectedClient sender, TMessage message, CancellationToken cancellationToken)
+        {
+            var valueTasks = _messageHandlerFactory.GetHandlersFor<TMessage>()
+                .Select(handler => handler.HandleAsync(sender, message, cancellationToken));
+
+            return AsyncHelpers.WhenAll(valueTasks);
+        }
+    }
+}
