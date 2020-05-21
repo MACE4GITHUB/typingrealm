256eaf1a01f20fef78797d20beb85ce07de6ba4c
Ivan Zyranau <ewancoder@gmail.com>
2020-05-21 15:02:05 +0300

    Add AutoMoqData attribute for testing, update packages

---
+++ TypingRealm.Testing/InlineAutoMoqDataAttribute.cs
@@ -0,0 +1,9 @@
+﻿using AutoFixture.Xunit2;
+
+namespace TypingRealm.Testing
+{
+    public class InlineAutoMoqDataAttribute : InlineAutoDataAttribute
+    {
+        public InlineAutoMoqDataAttribute(params object[] objects) : base(new AutoMoqDataAttribute(), objects) { }
+    }
+}
