c96e7da8d72ff18622f3181a8776048ca4d9f8aa
Ivan Zyranau <ewancoder@gmail.com>
2020-05-22 02:29:03 +0300

    Add IUpdater
    It sends update to the client. Concrete implementation will be based on
    Game Domain logic.

---
+++ TypingRealm.Messaging/Updating/IUpdater.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TypingRealm.Messaging.Updating
+{
+    /// <summary>
+    /// Sends update to client (after the client has been marked for an update).
+    /// </summary>
+    public interface IUpdater
+    {
+        /// <summary>
+        /// Sends update to client.
+        /// </summary>
+        /// <param name="client">Connected client.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        ValueTask SendUpdateAsync(ConnectedClient client, CancellationToken cancellationToken);
+    }
+}
