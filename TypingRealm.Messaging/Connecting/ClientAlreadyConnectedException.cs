f0f8f3c832a2de587122f368ade58bd4510bdac8
Ivan Zyranau <ewancoder@gmail.com>
2020-05-22 01:26:44 +0300

    Add documentation to ClientAlreadyConnectedException

--- TypingRealm.Messaging/Connecting/ClientAlreadyConnectedException.cs
+++ TypingRealm.Messaging/Connecting/ClientAlreadyConnectedException.cs
@@ -2,6 +2,10 @@
 
 namespace TypingRealm.Messaging.Connecting
 {
+    /// <summary>
+    /// This exception is thrown from <see cref="ConnectedClientStore"/> when we
+    /// try to add <see cref="ConnectedClient"/> that already exists in the store.
+    /// </summary>
     public sealed class ClientAlreadyConnectedException : Exception
     {
         public ClientAlreadyConnectedException(ConnectedClient connectedClient)
