31e6a737820bebcc55d6812c3bc6e55404446063
Ivan Zyranau <ewancoder@gmail.com>
2020-05-21 22:48:57 +0300

    Add UpdateDetector

---
+++ Tests/TypingRealm.Messaging.Tests/Updating/UpdateDetectorTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TypingRealm.Messaging.Updating;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Tests.Updating
+{
+    public class UpdateDetectorTests : TestsBase
+    {
+        [Theory, AutoMoqData]
+        public void ShouldMarkGroupForUpdate(
+            string[] groups,
+            UpdateDetector sut)
+        {
+            foreach (var group in groups)
+            {
+                sut.MarkForUpdate(group);
+            }
+
+            var result = sut.PopMarked();
+            foreach (var group in groups)
+            {
+                Assert.Contains(group, result);
+            }
+        }
+
+        [Theory, AutoMoqData]
+        public void ShouldNotMarkTheSameGroupTwice(
+            string group,
+            UpdateDetector sut)
+        {
+            sut.MarkForUpdate(group);
+            sut.MarkForUpdate(group);
+
+            Assert.Single(sut.PopMarked());
+        }
+
+        [Theory, AutoMoqData]
+        public void ShouldPopAllMarkedGroups(
+            string[] groups,
+            UpdateDetector sut)
+        {
+            foreach (var group in groups)
+            {
+                sut.MarkForUpdate(group);
+            }
+
+            Assert.Equal(3, sut.PopMarked().Count());
+            Assert.Empty(sut.PopMarked());
+
+            sut.MarkForUpdate(groups[0]);
+            Assert.Equal(groups[0], sut.PopMarked().Single());
+            Assert.Empty(sut.PopMarked());
+        }
+
+        // This test is likely useless.
+        [Theory, AutoMoqData]
+        public async Task ShouldMarkThreadSafely(UpdateDetector sut)
+        {
+            var tasks = new List<Task>(100);
+            for (var i = 1; i <= 100; i++)
+            {
+                var group = Create<string>();
+                tasks.Add(new Task(() => sut.MarkForUpdate(group)));
+                tasks.Add(new Task(() => sut.MarkForUpdate(group)));
+            }
+
+            Parallel.ForEach(tasks, t => t.Start());
+            await Task.WhenAll(tasks);
+
+            Assert.Equal(100, sut.PopMarked().Count());
+        }
+    }
+}
---
+++ TypingRealm.Messaging/Updating/UpdateDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TypingRealm.Messaging.Updating
+{
+    public sealed class UpdateDetector : IUpdateDetector
+    {
+        private readonly object _lock = new object();
+        private readonly HashSet<string> _marked
+            = new HashSet<string>();
+
+        public void MarkForUpdate(string group)
+        {
+            lock (_lock)
+            {
+                _marked.Add(group);
+            }
+        }
+
+        public IEnumerable<string> PopMarked()
+        {
+            lock (_lock)
+            {
+                var values = _marked.ToList();
+                _marked.Clear();
+                return values;
+            }
+        }
+    }
+}
