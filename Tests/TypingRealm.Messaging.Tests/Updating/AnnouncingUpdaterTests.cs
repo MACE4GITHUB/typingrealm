c6fef71d4323247ad86ca0a4eb4aa8bac90842fd
Ivan Zyranau <ewancoder@gmail.com>
2020-05-24 02:06:52 +0300

    Add AnnouncingUpdater as a simple testing updater
    This updater will just send "Update" text in Announce message.

---
+++ Tests/TypingRealm.Messaging.Tests/Updating/AnnouncingUpdaterTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using Moq;
+using TypingRealm.Messaging.Messages;
+using TypingRealm.Messaging.Updating;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Tests.Updating
+{
+    public class AnnouncingUpdaterTests : TestsBase
+    {
+        [Theory, AutoMoqData]
+        public async Task ShouldSendAnnounceMessage(
+            [Frozen]Mock<IConnection> connection,
+            ConnectedClient client,
+            AnnouncingUpdater sut)
+        {
+            using var cts = new CancellationTokenSource();
+            await sut.SendUpdateAsync(client, cts.Token);
+
+            connection.Verify(x => x.SendAsync(It.IsAny<Announce>(), cts.Token));
+        }
+    }
+}
---
+++ TypingRealm.Messaging/Updating/AnnouncingUpdater.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using TypingRealm.Messaging.Messages;
+
+namespace TypingRealm.Messaging.Updating
+{
+    /// <summary>
+    /// Simple updater that just sends "Update" announce message to the client.
+    /// Used for testing purposes.
+    /// </summary>
+    public sealed class AnnouncingUpdater : IUpdater
+    {
+        public ValueTask SendUpdateAsync(ConnectedClient client, CancellationToken cancellationToken)
+        {
+            return client.Connection.SendAsync(new Announce("Update"), cancellationToken);
+        }
+    }
+}
