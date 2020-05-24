9cbbb766c17acb69da3e52b4c6fb1304c3143ec5
Ivan Zyranau <ewancoder@gmail.com>
2020-05-24 03:38:14 +0300

    Add AddSerializationCore registration method
    It's used to register MessageTypeCache / MessageTypeCacheBuilder and all
    the messages from TypingRealm.Messaging.Core assembly.

---
+++ Tests/TypingRealm.Messaging.Serialization.Core.Tests/RegistrationExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using TypingRealm.Messaging.Messages;
+using Xunit;
+
+namespace TypingRealm.Messaging.Serialization.Tests
+{
+    public class RegistrationExtensionsTests
+    {
+        [Fact]
+        public void AddSerializationCore_ShouldSetValidServices()
+        {
+            var services = new ServiceCollection();
+            var sut = services.AddSerializationCore();
+
+            Assert.Equal(services, sut.Services);
+        }
+
+        [Fact]
+        public void AddSerializationCore_ShouldAddAllMessagesFromMessagingAssemblyAndOnlyThem()
+        {
+            var services = new ServiceCollection();
+            _ = services.AddSerializationCore();
+
+            var messages = services.BuildServiceProvider()
+                .GetRequiredService<IMessageTypeCache>()
+                .GetAllTypes()
+                .Select(x => x.Value)
+                .ToList();
+
+            var asmMessages = typeof(Announce).Assembly
+                .GetTypes()
+                .Where(x => x.GetCustomAttribute<MessageAttribute>() != null)
+                .ToList();
+
+            Assert.Equal(asmMessages.Count, messages.Count);
+            foreach (var asmMessage in asmMessages)
+            {
+                Assert.Contains(asmMessage, messages);
+            }
+        }
+    }
+}
---
+++ TypingRealm.Messaging.Serialization.Core/RegistrationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TypingRealm.Messaging.Messages;
+
+namespace TypingRealm.Messaging.Serialization
+{
+    public static class RegistrationExtensions
+    {
+        /// <summary>
+        /// Registers IMessageTypeCache as singleton and returns new instance of
+        /// <see cref="MessageTypeCacheBuilder"/> class that has core Messaging
+        /// messages registered in it. This builder will be used it the time
+        /// when <see cref="MessageTypeCache"/> is first requested.
+        /// </summary>
+        public static MessageTypeCacheBuilder AddSerializationCore(this IServiceCollection services)
+        {
+            return new MessageTypeCacheBuilder(services)
+                .AddMessageTypesFromAssembly(typeof(Disconnect).Assembly);
+        }
+    }
+}
