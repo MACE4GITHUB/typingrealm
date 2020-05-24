8afc79929f79947eafa9b3250841ef93dbdc4e8c
Ivan Zyranau <ewancoder@gmail.com>
2020-05-25 02:04:05 +0300

    Add protobuf serialization RegistrationExtensions

---
+++ Tests/TypingRealm.Messaging.Serialization.Protobuf.Core.Tests/RegistrationExtensionsTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using ProtoBuf.Meta;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Serialization.Protobuf.Tests
+{
+    public class AMessage
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class BMessage
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class RegistrationExtensionsTests
+    {
+        [Theory, AutoMoqData]
+        public void AddProtobuf_ShouldRegisterProtobufConnectionFactory()
+        {
+            var services = new ServiceCollection();
+            var sut = new MessageTypeCacheBuilder(services);
+
+            sut.AddProtobuf();
+
+            var provider = services.BuildServiceProvider();
+            AssertRegisteredTransient<IProtobufConnectionFactory, ProtobufConnectionFactory>(provider);
+        }
+
+        [Theory, AutoMoqData]
+        public void AddProtobuf_ShouldAddAllRegisteredTypesToProtobufRuntimeTypeModel_WithAllProperties_OrderedByName()
+        {
+            var services = new ServiceCollection();
+            var sut = new MessageTypeCacheBuilder(services);
+
+            sut.AddProtobuf();
+            sut.AddMessageType(typeof(AMessage));
+            sut.AddMessageType(typeof(BMessage));
+
+            var provider = services.BuildServiceProvider();
+            var types = RuntimeTypeModel.Default.GetTypes().Cast<MetaType>().ToList();
+            Assert.Empty(types);
+
+            _ = provider.GetRequiredService<IMessageTypeCache>();
+            types = RuntimeTypeModel.Default.GetTypes().Cast<MetaType>().ToList();
+            Assert.Equal(2, types.Count);
+            MetaType type;
+
+            var type1 = types.Single(t => t.Type == typeof(AMessage));
+            var type2 = types.Single(t => t.Type == typeof(BMessage));
+
+            var type1Members = type1.GetFields().ToList();
+
+            var member1 = type1Members.Single(t => t.FieldNumber == 1);
+            var member2 = type1Members.Single(t => t.FieldNumber == 2);
+
+            Assert.Equal(typeof(int), member1.MemberType);
+            Assert.Equal(nameof(AMessage.Age), member1.Name);
+            Assert.Equal(typeof(string), member2.MemberType);
+            Assert.Equal(nameof(AMessage.Name), member2.Name);
+        }
+
+        private void AssertRegisteredTransient<TInterface, TImplementation>(IServiceProvider provider)
+        {
+            var implementation = provider.GetRequiredService<TInterface>();
+            var implementation2 = provider.GetRequiredService<TInterface>();
+
+            using var scope = provider.CreateScope();
+            var implementation3 = scope.ServiceProvider.GetRequiredService<TInterface>();
+
+            Assert.NotNull(implementation);
+            Assert.IsType<TImplementation>(implementation);
+            Assert.IsType<TImplementation>(implementation2);
+            Assert.IsType<TImplementation>(implementation3);
+            Assert.NotEqual(implementation, implementation2);
+            Assert.NotEqual(implementation, implementation3);
+        }
+    }
+}
---
+++ TypingRealm.Messaging.Serialization.Protobuf.Core/RegistrationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using ProtoBuf.Meta;
+
+namespace TypingRealm.Messaging.Serialization.Protobuf
+{
+    public static class RegistrationExtensions
+    {
+        /// <summary>
+        /// Registers protobuf stream connection factory and adds a post-build
+        /// action to register all messages in protobuf RuntimeTypeModel.
+        /// </summary>
+        public static MessageTypeCacheBuilder AddProtobuf(this MessageTypeCacheBuilder builder)
+        {
+            builder.Services.AddTransient<IProtobufConnectionFactory, ProtobufConnectionFactory>();
+            builder.AddPostBuildAction(messageTypes => RegisterProtobufMessages(messageTypes));
+
+            return builder;
+        }
+
+        private static void RegisterProtobufMessages(IMessageTypeCache messageTypes)
+        {
+            foreach (var type in messageTypes.GetAllTypes())
+            {
+                RuntimeTypeModel.Default.Add(type.Value, false)
+                    .Add(type.Value
+                        .GetProperties()
+                        .Select(property => property.Name)
+                        .OrderBy(name => name)
+                        .ToArray());
+            }
+        }
+    }
+}
