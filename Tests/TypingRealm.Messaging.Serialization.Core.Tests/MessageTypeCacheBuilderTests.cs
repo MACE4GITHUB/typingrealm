19948b1770e1e1ce005f9ae4c5a06340d94cfd49
Ivan Zyranau <ewancoder@gmail.com>
2020-05-24 03:06:42 +0300

    Add MessageTypeCacheBuilder
    This is a helper class to simplify dependency injection configuration.
    
    We need sometimes to both register dependencies in IServiceCollection as
    well as register custom serialization types for serializer to know how
    to map them. Hence we are combining these two concerns in this
    dependency builder.
    
    Also libraries such as Protobuf that depend on static runtime classes to
    register types - can add custom Action<IMessageTypeCache> post-build
    actions that will be executed in the order of registration after the
    initial build of MessageTypeCache and before it is returned to the
    requested party.
    
    The IMessageTypeCache is registered as singleton (hardcoded, not
    changeable). However, in future it's possible to include the flag on
    MessageTypeCacheBuilder indicating whether to build it as singleton or
    no.

---
+++ Tests/TypingRealm.Messaging.Serialization.Core.Tests/MessageTypeCacheBuilderTests.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Serialization.Tests
+{
+    public class MessageTypeCacheBuilderTests
+    {
+        [Fact]
+        public void ShouldAssignServiceCollectionToProperty()
+        {
+            var services = new ServiceCollection();
+            var sut = new MessageTypeCacheBuilder(services);
+
+            Assert.Equal(services, sut.Services);
+        }
+
+        [Fact]
+        public void ShouldRegisterMessageTypeCacheAsSingleton()
+        {
+            var services = new ServiceCollection();
+            _ = new MessageTypeCacheBuilder(services);
+
+            var provider = services.BuildServiceProvider();
+            var cache1 = provider.GetRequiredService<IMessageTypeCache>();
+            var cache2 = provider.GetRequiredService<IMessageTypeCache>();
+            using var scope = provider.CreateScope();
+            var cache3 = scope.ServiceProvider.GetRequiredService<IMessageTypeCache>();
+
+            Assert.Equal(cache1, cache2);
+            Assert.Equal(cache1, cache3);
+        }
+
+        [Fact]
+        public void ShouldExecuteAllRegisteredPostBuildActions_InOrder_SingleTime_WhenFirstTimeRequired()
+        {
+            var services = new ServiceCollection();
+            var builder = new MessageTypeCacheBuilder(services);
+            var actions = new Dictionary<int, IMessageTypeCache>();
+            builder.AddPostBuildAction(cache => actions.Add(1, cache));
+            builder.AddPostBuildAction(cache => actions.Add(3, cache));
+            builder.AddPostBuildAction(cache => actions.Add(2, cache));
+
+            Assert.Empty(actions);
+
+            var provider = services.BuildServiceProvider();
+            Assert.Empty(actions);
+
+            var cache = provider.GetRequiredService<IMessageTypeCache>();
+            Assert.Equal(3, actions.Count);
+
+            provider.GetRequiredService<IMessageTypeCache>();
+            Assert.Equal(3, actions.Count);
+
+            Assert.Equal(1, actions.Keys.ToList()[0]);
+            Assert.Equal(3, actions.Keys.ToList()[1]);
+            Assert.Equal(2, actions.Keys.ToList()[2]);
+            Assert.Equal(cache, actions[1]);
+            Assert.Equal(cache, actions[2]);
+            Assert.Equal(cache, actions[3]);
+        }
+
+        [Fact]
+        public void ShouldHaveNoMessagesByDefault()
+        {
+            var services = new ServiceCollection();
+            _ = new MessageTypeCacheBuilder(services);
+
+            var cache = services.BuildServiceProvider().GetRequiredService<IMessageTypeCache>();
+            Assert.Empty(cache.GetAllTypes());
+        }
+
+        [Fact]
+        public void ShouldAddMessages()
+        {
+            var services = new ServiceCollection();
+            _ = new MessageTypeCacheBuilder(services)
+                .AddMessageType(typeof(TestMessage))
+                .AddMessageType(typeof(string))
+                .AddMessageType(typeof(object));
+
+            var cache = services.BuildServiceProvider().GetRequiredService<IMessageTypeCache>();
+            Assert.Equal(3, cache.GetAllTypes().Count());
+            Assert.Contains(typeof(TestMessage), cache.GetAllTypes().Select(x => x.Value));
+            Assert.Contains(typeof(string), cache.GetAllTypes().Select(x => x.Value));
+            Assert.Contains(typeof(object), cache.GetAllTypes().Select(x => x.Value));
+        }
+    }
+}
---
+++ TypingRealm.Messaging.Serialization.Core/MessageTypeCacheBuilder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TypingRealm.Messaging.Serialization
+{
+    /// <summary>
+    /// Registers <see cref="MessageTypeCache"/> as singleton.
+    /// </summary>
+    public sealed class MessageTypeCacheBuilder
+    {
+        private readonly HashSet<Type> _messageTypes = new HashSet<Type>();
+        private readonly List<Action<MessageTypeCache>> _postBuildActions
+            = new List<Action<MessageTypeCache>>();
+
+        /// <summary>
+        /// ServiceCollection instance where this MessageTypeCache is registered
+        /// as singleton.
+        /// </summary>
+        public IServiceCollection Services { get; }
+
+        public MessageTypeCacheBuilder(IServiceCollection services)
+        {
+            Services = services;
+
+            services.AddSingleton<IMessageTypeCache>(provider => Build());
+        }
+
+        /// <summary>
+        /// Adds message type to the <see cref="IMessageTypeCache"/>.
+        /// </summary>
+        public MessageTypeCacheBuilder AddMessageType(Type messageType)
+        {
+            _messageTypes.Add(messageType);
+            return this;
+        }
+
+        /// <summary>
+        /// Adds an action that will be executed when the instance of
+        /// <see cref="IMessageTypeCache"/> will be first constructed.
+        /// </summary>
+        public void AddPostBuildAction(Action<IMessageTypeCache> action)
+            => _postBuildActions.Add(action);
+
+        /// <summary>
+        /// Builds an instance of <see cref="MessageTypeCache"/>. This method
+        /// should be called only once when the container first requests the
+        /// singleton instance.
+        /// </summary>
+        private MessageTypeCache Build()
+        {
+            var cache = new MessageTypeCache(_messageTypes);
+
+            foreach (var action in _postBuildActions)
+            {
+                action(cache);
+            }
+
+            return cache;
+        }
+    }
+}
