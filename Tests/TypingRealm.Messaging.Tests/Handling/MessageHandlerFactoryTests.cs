2aea77065f32d04a578d4e89d56d9362227a2f5f
Ivan Zyranau <ewancoder@gmail.com>
2020-05-25 02:37:28 +0300

    Add MessageHandlerFactory that uses IServiceProvider
    This message handler factory is a service locator that uses .NET Core DI
    container to resolve instances of IMessageHandler<T>. It may be
    anti-pattern and it may not work so well in ASP.NET Core scenario (with
    scoped services, since the DI will be disposed after Startup has been
    build). We need to test it and to come up with better solution if any.

---
+++ Tests/TypingRealm.Messaging.Tests/Handling/MessageHandlerFactoryTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoFixture;
+using AutoFixture.Xunit2;
+using Moq;
+using TypingRealm.Messaging.Handling;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Tests.Handling
+{
+    public class MessageHandlerFactoryTests : TestsBase
+    {
+        [Theory, AutoMoqData]
+        public void ShouldGetAllHandlers(
+            [Frozen]Mock<IServiceProvider> services,
+            MessageHandlerFactory sut)
+        {
+            var handlers = Fixture.CreateMany<IMessageHandler<TestMessage>>();
+            services.Setup(x => x.GetService(typeof(IEnumerable<IMessageHandler<TestMessage>>)))
+                .Returns(handlers);
+
+            var result = sut.GetHandlersFor<TestMessage>();
+
+            Assert.Equal(handlers, result);
+        }
+    }
+}
