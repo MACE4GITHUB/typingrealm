bc409efd8c7d9c489818358c20d164f4bed49376
Ivan Zyranau <ewancoder@gmail.com>
2020-05-26 02:08:49 +0300

    Add tests for ClientAlreadyConnectedException

---
+++ Tests/TypingRealm.Messaging.Tests/Connecting/ClientAlreadyConnectedExceptionTests.cs
@@ -0,0 +1,24 @@
+﻿using AutoFixture.Xunit2;
+using TypingRealm.Messaging.Connecting;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Tests.Connecting
+{
+    public class ClientAlreadyConnectedExceptionTests
+    {
+        [Theory, AutoMoqData]
+        public void ShouldContainClientIdInMessage(
+            [Frozen]ConnectedClient client,
+            ClientAlreadyConnectedException sut)
+        {
+            Assert.Contains(client.ClientId, sut.Message);
+        }
+
+        [Theory, AutoMoqData]
+        public void ShouldContainAlreadyConnectedMessage(ClientAlreadyConnectedException sut)
+        {
+            Assert.Contains("already connected", sut.Message);
+        }
+    }
+}
