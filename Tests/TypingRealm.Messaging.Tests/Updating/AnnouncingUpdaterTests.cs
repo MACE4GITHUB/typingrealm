3bb289acf0a3330d706ea52f2b47ea4fc70fee59
Ivan Zyranau <ewancoder@gmail.com>
2020-05-28 02:46:26 +0300

    Fix bug in AnnouncingUpdaterTests
    This bug was introduced during test code cleanup & refactor.
    ConnectedClient needs to be an argument of the method in order to inject
    IConnection into it.

--- Tests/TypingRealm.Messaging.Tests/Updating/AnnouncingUpdaterTests.cs
+++ Tests/TypingRealm.Messaging.Tests/Updating/AnnouncingUpdaterTests.cs
@@ -13,9 +13,10 @@ namespace TypingRealm.Messaging.Tests.Updating
         [Theory, AutoMoqData]
         public async Task ShouldSendAnnounceMessage(
             [Frozen]Mock<IConnection> connection,
+            ConnectedClient client,
             AnnouncingUpdater sut)
         {
-            await sut.SendUpdateAsync(Create<ConnectedClient>(), Cts.Token);
+            await sut.SendUpdateAsync(client, Cts.Token);
 
             connection.Verify(x => x.SendAsync(It.IsAny<Announce>(), Cts.Token));
         }
