e056844d5b0876b6ef1505755ecbbe711e1e20e4
Ivan Zyranau <ewancoder@gmail.com>
2020-05-28 16:09:13 +0300

    Add LockExtensions - UseWaitAsync method
    It's used to acquire the lock and release it within Using block.

---
+++ Tests/TypingRealm.Messaging.Core.Tests/Connections/LockExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using TypingRealm.Messaging.Connections;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Tests.Connections
+{
+    public class LockExtensionsTests : TestsBase
+    {
+        [Theory, AutoMoqData]
+        public async Task UseWaitAsync_ShouldWaitForLock(ILock sut)
+        {
+            await sut.UseWaitAsync(Cts.Token);
+
+            Mock.Get(sut).Verify(x => x.WaitAsync(Cts.Token));
+        }
+
+        [Theory, AutoMoqData]
+        public async Task UseWaitAsync_ShouldReturnDisposableThatReleasesOnDispose(
+            Mock<ILock> sut)
+        {
+            var result = await sut.Object.UseWaitAsync(Cts.Token);
+
+            sut.Verify(x => x.ReleaseAsync(Cts.Token), Times.Never);
+
+            await result.DisposeAsync();
+            sut.Verify(x => x.ReleaseAsync(Cts.Token), Times.Once);
+
+            // Should not call dispose twice.
+            await result.DisposeAsync();
+            sut.Verify(x => x.ReleaseAsync(Cts.Token), Times.Once);
+        }
+    }
+}
---
+++ TypingRealm.Messaging.Core/Connections/LockExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TypingRealm.Messaging.Connections
+{
+    public static class LockExtensions
+    {
+        /// <summary>
+        /// Waits to acquire the lock and returns <see cref="IAsyncDisposable"/>
+        /// instance after the lock is acquired, which can be disposed in order
+        /// to release lock instance.
+        /// </summary>
+        public static async ValueTask<IAsyncDisposable> UseWaitAsync(this ILock @lock, CancellationToken cancellationToken)
+        {
+            await @lock.WaitAsync(cancellationToken).ConfigureAwait(false);
+
+            return new LockReleaseWrapper(@lock, cancellationToken);
+        }
+
+        private sealed class LockReleaseWrapper : AsyncManagedDisposable
+        {
+            private readonly ILock _lock;
+            private readonly CancellationToken _cancellationToken;
+
+            public LockReleaseWrapper(ILock @lock, CancellationToken cancellationToken)
+            {
+                _lock = @lock;
+                _cancellationToken = cancellationToken;
+            }
+
+            protected override ValueTask DisposeManagedResourcesAsync()
+                => _lock.ReleaseAsync(_cancellationToken);
+        }
+    }
+}
