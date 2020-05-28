6d9b002830fee27bb3981906a2cda2302831b81b
Ivan Zyranau <ewancoder@gmail.com>
2020-05-28 16:51:21 +0300

    Add ConnectionExtensions to simplify LockingConnection usage

---
+++ TypingRealm.Messaging.Core/Connections/ConnectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace TypingRealm.Messaging.Connections
+{
+    public static class ConnectionExtensions
+    {
+        public static IConnection WithLocking(this IConnection connection, ILock sendLock, ILock receiveLock)
+        {
+            return new LockingConnection(connection, sendLock, receiveLock);
+        }
+    }
+}
