ddd6ded580670ce0a86102fced542206f8383d24
Ivan Zyranau <ewancoder@gmail.com>
2020-05-29 02:21:02 +0300

    Add NotificatorConnection
    This is needed to be able to notify about received messages from
    external systems where it's not possible to convert it to IConnection,
    for example SignalR hubs.

---
+++ Tests/TypingRealm.Messaging.Core.Tests/Connections/NotificatorConnectionTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using Moq;
+using TypingRealm.Messaging.Connections;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Tests.Connections
+{
+    public class NotificatorConnectionTests : TestsBase
+    {
+        [Theory, AutoMoqData]
+        public async Task ShouldUseConnectionToSendMessages(
+            [Frozen]Mock<IMessageSender> messageSender,
+            TestMessage message,
+            NotificatorConnection sut)
+        {
+            await sut.SendAsync(message, Cts.Token);
+
+            messageSender.Verify(x => x.SendAsync(message, Cts.Token));
+        }
+
+        [Theory, AutoMoqData]
+        public async Task ShouldNotUseUnderlyingConnectionToWaitForMessages(
+            [Frozen]Mock<IConnection> connection,
+            [Frozen]Notificator notificator,
+            NotificatorConnection sut)
+        {
+            notificator.NotifyReceived(Create<object>());
+            await sut.ReceiveAsync(Cts.Token);
+
+            connection.Verify(x => x.ReceiveAsync(Cts.Token), Times.Never);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task ShouldReturnMessageSynchronouslyIfAlreadyReceived(
+            [Frozen]Notificator notificator,
+            NotificatorConnection sut)
+        {
+            notificator.NotifyReceived(Create<object>());
+            Assert.True(sut.ReceiveAsync(Cts.Token).IsCompletedSuccessfully);
+
+            var message = Create<object>();
+            notificator.NotifyReceived(message);
+            Assert.Equal(message, sut.ReceiveAsync(Cts.Token).Result);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task ShouldReturnMessageWhenNotified(
+            [Frozen]Notificator notificator,
+            TestMessage message,
+            NotificatorConnection sut)
+        {
+            var result = sut.ReceiveAsync(Cts.Token);
+            await Wait();
+
+            Assert.False(result.IsCompleted);
+
+            notificator.NotifyReceived(message);
+            var received = await result;
+
+            Assert.Equal(message, received);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task ShouldCancel_WhenCancellationRequested(NotificatorConnection sut)
+        {
+            var result = sut.ReceiveAsync(Cts.Token);
+            await Wait();
+
+            Assert.False(result.IsCompleted);
+
+            Cts.Cancel();
+            await Assert.ThrowsAsync<OperationCanceledException>(() => result.AsTask());
+        }
+
+        [Theory, AutoMoqData]
+        public async Task ShouldUnsubscribeAfterNotification(
+            [Frozen]Notificator notificator,
+            NotificatorConnection sut)
+        {
+            int GetSubscriptionCount()
+            {
+                return (typeof(Notificator)
+                .GetField(nameof(Notificator.Received), BindingFlags.Instance | BindingFlags.NonPublic)?
+                .GetValue(notificator) as Action)?.GetInvocationList()?.Length ?? 0;
+            }
+
+            Assert.Equal(0, GetSubscriptionCount());
+
+            var result = sut.ReceiveAsync(Cts.Token);
+            await Wait();
+
+            Assert.Equal(1, GetSubscriptionCount());
+
+            notificator.NotifyReceived(Create<object>());
+            await result;
+
+            Assert.Equal(0, GetSubscriptionCount());
+        }
+
+        [Theory, AutoMoqData]
+        public async Task ShouldUnsubscribeAfterCancellation(
+            [Frozen]Notificator notificator,
+            NotificatorConnection sut)
+        {
+            int GetSubscriptionCount()
+            {
+                return (typeof(Notificator)
+                .GetField(nameof(Notificator.Received), BindingFlags.Instance | BindingFlags.NonPublic)?
+                .GetValue(notificator) as Action)?.GetInvocationList()?.Length ?? 0;
+            }
+
+            Assert.Equal(0, GetSubscriptionCount());
+
+            var result = sut.ReceiveAsync(Cts.Token);
+            await Wait();
+
+            Assert.Equal(1, GetSubscriptionCount());
+
+            Cts.Cancel();
+            await SwallowAnyAsync(result.AsTask());
+
+            Assert.Equal(0, GetSubscriptionCount());
+        }
+    }
+}
