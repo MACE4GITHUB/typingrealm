19ec6efab67164b4d53754134394b2b4ca1ecb08
Ivan Zyranau <ewancoder@gmail.com>
2020-05-29 02:46:49 +0300

    Add JsonSerializedMessage message
    It will be used for Json serialization data payload wrapper.

---
+++ TypingRealm.Messaging.Serialization.Core/Json/JsonSerializedMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace TypingRealm.Messaging.Serialization.Json
+{
+    [Message]
+    public sealed class JsonSerializedMessage
+    {
+#pragma warning disable CS8618
+        public JsonSerializedMessage() { }
+#pragma warning restore CS8618
+        public JsonSerializedMessage(string typeId, string json)
+        {
+            TypeId = typeId;
+            Json = json;
+        }
+
+        public string TypeId { get; set; }
+        public string Json { get; set; }
+    }
+}
