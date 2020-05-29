d9fab6917a53ab3b19f205d49ecfcd17fa42d6b6
Ivan Zyranau <ewancoder@gmail.com>
2020-05-29 03:19:38 +0300

    Add JsonConnectionFactory and registration extensions

---
+++ TypingRealm.Messaging.Serialization.Core/Json/RegistrationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TypingRealm.Messaging.Serialization.Json
+{
+    public static class RegistrationExtensions
+    {
+        /// <summary>
+        /// Registers <see cref="JsonConnectionFactory"/> that relies on
+        /// <see cref="IMessageTypeCache"/>. <see cref="Serialization.RegistrationExtensions.AddSerializationCore(IServiceCollection)"/>
+        /// should be called first.
+        /// </summary>
+        public static IServiceCollection AddJson(this IServiceCollection services)
+        {
+            return services.AddTransient<IJsonConnectionFactory, JsonConnectionFactory>();
+        }
+    }
+}
