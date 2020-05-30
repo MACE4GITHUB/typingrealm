bda00b3db8d5203df5a04e10189d5abe2248423b
Ivan Zyranau <ewancoder@gmail.com>
2020-05-30 03:22:31 +0300

    Add WithJson extension method for IConnection
    It uses IJsonConnectionFactory to create a JsonConnection wrapping the
    decorated connection inside it.

---
+++ Tests/TypingRealm.Messaging.Serialization.Core.Tests/Json/ConnectionExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using TypingRealm.Messaging.Serialization.Json;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Serialization.Tests.Json
+{
+    public class ConnectionExtensionsTests
+    {
+        [Theory, AutoMoqData]
+        public void WithJson_ShouldUseJsonConnectionFactoryToCreateJsonConnection(
+            IJsonConnectionFactory factory,
+            JsonConnection jsonConnection,
+            IConnection sut)
+        {
+            Mock.Get(factory).Setup(x => x.CreateJsonConnection(sut))
+                .Returns(jsonConnection);
+
+            var result = sut.WithJson(factory);
+
+            Assert.Equal(jsonConnection, result);
+        }
+    }
+}
---
+++ TypingRealm.Messaging.Serialization.Core/Json/ConnectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace TypingRealm.Messaging.Serialization.Json
+{
+    public static class ConnectionExtensions
+    {
+        public static JsonConnection WithJson(this IConnection connection, IJsonConnectionFactory jsonConnectionFactory)
+        {
+            return jsonConnectionFactory.CreateJsonConnection(connection);
+        }
+    }
+}
