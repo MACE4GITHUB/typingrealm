d3b8537231d62e8b0d052a22881abcd26068661d
Ivan Zyranau <ewancoder@gmail.com>
2020-05-31 02:26:45 +0300

    Add Domain.Core project and Join message there
    Join message will be used to join the server. For now we only ask the
    player to specify their player name so anybody can join with any name.

---
+++ Tests/TypingRealm.Domain.Core.Tests/MessagesTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Reflection;
+using TypingRealm.Domain.Messages;
+using TypingRealm.Messaging;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Domain.Tests
+{
+    public class MessagesTests : TestsBase
+    {
+        [Fact]
+        public void ShouldHaveTestsForAllMessages()
+        {
+            Assert.Equal(1, typeof(Join).Assembly.GetTypes().Count(
+                t => t.GetCustomAttribute<MessageAttribute>() != null));
+        }
+
+        [Fact]
+        public void JoinMessage()
+        {
+            AssertSerializable<Join>();
+
+            var sut = new Join
+            {
+                Name = "name"
+            };
+            Assert.Equal("name", sut.Name);
+
+            sut = new Join("name");
+            Assert.Equal("name", sut.Name);
+        }
+    }
+}
---
+++ TypingRealm.Domain.Core/Messages/Join.cs
@@ -0,0 +1,15 @@
+﻿using TypingRealm.Messaging;
+
+namespace TypingRealm.Domain.Messages
+{
+    [Message]
+    public sealed class Join
+    {
+#pragma warning disable CS8618
+        public Join() { }
+#pragma warning restore CS8618
+        public Join(string name) => Name = name;
+
+        public string Name { get; set; }
+    }
+}
