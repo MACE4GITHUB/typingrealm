b642bf0fc3baeab8ccc3e2d400453d31d8eae5de
Ivan Zyranau <ewancoder@gmail.com>
2020-06-01 20:02:07 +0300

    Inherit Identity from Primitive
    Identity is essentially a primitive with custom validation.

--- TypingRealm.Domain/Common/Identity.cs
+++ TypingRealm.Domain/Common/Identity.cs
@@ -3,52 +3,12 @@ using System.Collections.Generic;
 
 namespace TypingRealm.Domain.Common
 {
-    public abstract class Identity<TValue>
+    public abstract class Identity<TValue> : Primitive<TValue>
     {
-        protected Identity(TValue value)
+        protected Identity(TValue value) : base(value)
         {
             if (EqualityComparer<TValue>.Default.Equals(value, default))
                 throw new ArgumentException("Identity cannot have default value.", nameof(value));
-
-            Value = value;
         }
-
-        public TValue Value { get; }
-
-        public sealed override bool Equals(object? obj)
-        {
-            return GetType() == obj?.GetType() // Test that types are exactly the same.
-                && obj is Identity<TValue> other
-                && EqualityComparer<TValue>.Default.Equals(Value, other.Value);
-        }
-
-        public sealed override int GetHashCode()
-        {
-            return Value!.GetHashCode();
-        }
-
-        public override string? ToString()
-        {
-            return Value!.ToString();
-        }
-
-        public static bool operator ==(Identity<TValue> left, Identity<TValue> right)
-        {
-            if (left is null)
-            {
-                if (right is null)
-                    return true;
-
-                return false;
-            }
-
-            return left.Equals(right);
-        }
-
-        public static bool operator !=(Identity<TValue> left, Identity<TValue> right)
-            => !(left == right);
-
-        public static implicit operator TValue(Identity<TValue> identity)
-            => identity.Value;
     }
 }
