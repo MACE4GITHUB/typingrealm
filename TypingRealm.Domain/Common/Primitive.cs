178c5753391473b30bfc8f4fc2ce18707d730c42
Ivan Zyranau <ewancoder@gmail.com>
2020-06-01 03:13:35 +0300

    Add Primitive base class
    It's the same as Identity class except for constructor validation.
    It might be sensible to make Identity inherit Primitive.

---
+++ TypingRealm.Domain/Common/Primitive.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TypingRealm.Domain.Common
+{
+    public abstract class Primitive<TValue>
+    {
+        protected Primitive(TValue value)
+        {
+            if (!typeof(TValue).IsValueType && value is null)
+                throw new ArgumentNullException(nameof(value));
+
+            Value = value;
+        }
+
+        public TValue Value { get; }
+
+        public sealed override bool Equals(object? obj)
+        {
+            return GetType() == obj?.GetType() // Test that types are exactly the same.
+                && obj is Primitive<TValue> other
+                && EqualityComparer<TValue>.Default.Equals(Value, other.Value);
+        }
+
+        public sealed override int GetHashCode()
+        {
+            return Value!.GetHashCode();
+        }
+
+        public override string? ToString()
+        {
+            return Value!.ToString();
+        }
+
+        public static bool operator ==(Primitive<TValue> left, Primitive<TValue> right)
+        {
+            if (left is null)
+            {
+                if (right is null)
+                    return true;
+
+                return false;
+            }
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Primitive<TValue> left, Primitive<TValue> right)
+            => !(left == right);
+    }
+}
