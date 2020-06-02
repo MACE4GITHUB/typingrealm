c2afd2ed6bf8a81820fb678fd4c3c66c370988dd
Ivan Zyranau <ewancoder@gmail.com>
2020-06-02 18:06:33 +0300

    Add tests for Identity

---
+++ Tests/TypingRealm.Domain.Tests/Common/IdentityTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TypingRealm.Domain.Common;
+using Xunit;
+
+namespace TypingRealm.Domain.Tests.Common
+{
+    public class IdentityTests
+    {
+        public class TestIdentity : Identity
+        {
+            public TestIdentity(string value) : base(value)
+            {
+            }
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("        ")]
+        public void ShouldThrowWhenEmpty(string value)
+        {
+            Assert.Throws<ArgumentException>(
+                () => new TestIdentity(value));
+        }
+    }
+}
