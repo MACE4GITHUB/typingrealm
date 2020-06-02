ede71b70e86f9e9c8f1ae3c7d7f9a0b43d59f631
Ivan Zyranau <ewancoder@gmail.com>
2020-06-02 18:01:46 +0300

    Remove generic Identity class
    Primitive class covers most of cases, and non-generic string Identity
    class covers basic needs for identity (helper class). Furthermore,
    generic Identity class did not allow "0" value of int, however in some
    databases it might be a valid value, so the decision was made to get rid
    of this class.

--- TypingRealm.Domain/Common/Identity.cs
+++ TypingRealm.Domain/Common/Identity.cs
@@ -1,26 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace TypingRealm.Domain.Common
 {
     /// <summary>
     /// String identity.
     /// </summary>
-    public abstract class Identity : Identity<string>
+    public abstract class Identity : Primitive<string>
     {
         protected Identity(string value) : base(value)
         {
             if (value.Trim().Length == 0)
                 throw new ArgumentException("String identity cannot be empty.");
-        }
-    }
-
-    public abstract class Identity<TValue> : Primitive<TValue>
-    {
-        protected Identity(TValue value) : base(value)
-        {
-            if (EqualityComparer<TValue>.Default.Equals(value, default))
-                throw new ArgumentException("Identity cannot have default value.", nameof(value));
         }
     }
 }
