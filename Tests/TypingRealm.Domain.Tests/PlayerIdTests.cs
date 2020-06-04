3b653eeba3d14bb2fe39548309bcccb76a003860
Ivan Zyranau <ewancoder@gmail.com>
2020-06-04 10:59:10 +0300

    Add PlayerId tests

---
+++ Tests/TypingRealm.Domain.Tests/PlayerIdTests.cs
@@ -0,0 +1,14 @@
+﻿using TypingRealm.Domain.Common;
+using Xunit;
+
+namespace TypingRealm.Domain.Tests
+{
+    public class PlayerIdTests
+    {
+        [Theory, AutoDomainData]
+        public void ShouldBeIdentity(PlayerId playerId)
+        {
+            Assert.IsAssignableFrom<Identity>(playerId);
+        }
+    }
+}
