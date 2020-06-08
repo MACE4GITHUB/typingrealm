7e97fb391c65b64415009caf17e9b64862b4ec25
Ivan Zyranau <ewancoder@gmail.com>
2020-06-09 02:46:00 +0300

    Merge MessageSender extensions to ConnectionExtensions
    There is only one extension method in both of them and it's easier to
    browse the code when it's concentrated in one place logically.

--- Tests/TypingRealm.Messaging.Core.Tests/Connections/ConnectionExtensionsTests.cs
+++ Tests/TypingRealm.Messaging.Core.Tests/Connections/ConnectionExtensionsTests.cs
@@ -17,5 +17,23 @@ namespace TypingRealm.Messaging.Tests.Connections
             Assert.Equal(sendLock, GetPrivateField(sut, "_sendLock"));
             Assert.Equal(receiveLock, GetPrivateField(sut, "_receiveLock"));
         }
+
+        [Theory, AutoMoqData]
+        public void WithNotificator_ShouldSetNotificator(
+            Notificator notificator,
+            IMessageSender sut)
+        {
+            var connection = sut.WithNotificator(notificator);
+
+            Assert.Equal(notificator, GetPrivateField(connection, "_notificator"));
+        }
+
+        [Theory, AutoMoqData]
+        public void WithNotificator_ShouldSetMessageSender(IMessageSender sut)
+        {
+            var connection = sut.WithNotificator(Create<Notificator>());
+
+            Assert.Equal(sut, GetPrivateField(connection, "_messageSender"));
+        }
     }
 }
--- TypingRealm.Messaging.Core/Connections/ConnectionExtensions.cs
+++ TypingRealm.Messaging.Core/Connections/ConnectionExtensions.cs
@@ -2,9 +2,24 @@
 {
     public static class ConnectionExtensions
     {
-        public static LockingConnection WithLocking(this IConnection connection, ILock sendLock, ILock receiveLock)
+        /// <summary>
+        /// Adds locking to connection so that sending and receiving could be
+        /// done only via single thread at a time.
+        /// </summary>
+        public static LockingConnection WithLocking(
+            this IConnection connection, ILock sendLock, ILock receiveLock)
         {
             return new LockingConnection(connection, sendLock, receiveLock);
+        }
+
+        /// <summary>
+        /// Adds receiving capabilities to <see cref="IMessageSender"/> so that
+        /// it can work as <see cref="IConnection"/> using <see cref="Notificator"/>.
+        /// </summary>
+        public static NotificatorConnection WithNotificator(
+            this IMessageSender messageSender, Notificator notificator)
+        {
+            return new NotificatorConnection(messageSender, notificator);
         }
     }
 }
