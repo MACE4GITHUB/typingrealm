418e5c43d67f0cb43dccbda87f552d52bf751d83
Ivan Zyranau <ewancoder@gmail.com>
2020-06-09 02:35:52 +0300

    Add documentation comment to AddDomainCore registration method

--- TypingRealm.Domain.Core/RegistrationExtensions.cs
+++ TypingRealm.Domain.Core/RegistrationExtensions.cs
@@ -5,6 +5,9 @@ namespace TypingRealm.Domain
 {
     public static class RegistrationExtensions
     {
+        /// <summary>
+        /// Adds all domain messages to <see cref="MessageTypeCacheBuilder"/>.
+        /// </summary>
         public static MessageTypeCacheBuilder AddDomainCore(this MessageTypeCacheBuilder builder)
             => builder.AddMessageTypesFromAssembly(typeof(Join).Assembly);
     }
