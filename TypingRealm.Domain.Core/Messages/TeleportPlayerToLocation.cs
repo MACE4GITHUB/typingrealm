5f145ec487543eff9ccc6b0ad5111e4fd54e1fbb
Ivan Zyranau <ewancoder@gmail.com>
2020-06-10 09:55:40 +0300

    Add simple TeleportPlayerToLocation functionality for testing
    This message is mostly for testing messaging/updating implementation.

---
+++ TypingRealm.Domain.Core/Messages/TeleportPlayerToLocation.cs
@@ -0,0 +1,20 @@
+﻿using TypingRealm.Messaging;
+
+namespace TypingRealm.Domain.Messages
+{
+    [Message]
+    public sealed class TeleportPlayerToLocation
+    {
+#pragma warning disable CS8618
+        public TeleportPlayerToLocation() { }
+#pragma warning restore CS8618
+        public TeleportPlayerToLocation(string playerId, string locationId)
+        {
+            PlayerId = playerId;
+            LocationId = locationId;
+        }
+
+        public string PlayerId { get; set; }
+        public string LocationId { get; set; }
+    }
+}
