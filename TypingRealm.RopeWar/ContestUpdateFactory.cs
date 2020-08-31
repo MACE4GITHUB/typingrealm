7423bb79a92f0e23aae24fe51a80b2c82a7879c5
Ivan Zyranau <ewancoder@gmail.com>
2020-08-31 03:53:20 +0300

    Send update even when client is just a spectator

--- TypingRealm.RopeWar/ContestUpdateFactory.cs
+++ TypingRealm.RopeWar/ContestUpdateFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using TypingRealm.Messaging.Connecting;
 using TypingRealm.Messaging.Updating;
 
 namespace TypingRealm.RopeWar
@@ -7,19 +8,33 @@ namespace TypingRealm.RopeWar
     public sealed class ContestUpdateFactory : IUpdateFactory
     {
         private readonly IContestStore _contestStore;
+        private readonly IConnectedClientStore _connectedClients;
 
-        public ContestUpdateFactory(IContestStore contestStore)
+        public ContestUpdateFactory(
+            IContestStore contestStore,
+            IConnectedClientStore connectedClients)
         {
             _contestStore = contestStore;
+            _connectedClients = connectedClients;
         }
 
         public object GetUpdateFor(string clientId)
         {
             var contest = _contestStore.FindByContestantId(clientId);
 
-            // TODO: Return spectator update here, do not throw an exception. The client connected but did not join the contest.
             if (contest == null)
-                throw new InvalidOperationException($"Contest is not found for contestant {clientId}.");
+            {
+                // This is a bit of a hack. Either pass ConnectedClient to this method,
+                // or have a domain-specific ISpectatorsStore that will be filled by ConnectHook.
+                var client = _connectedClients.Find(clientId);
+                if (client == null)
+                    throw new InvalidOperationException("Client is not connected already.");
+
+                var contestId = client.Group;
+                contest = _contestStore.Find(contestId);
+                if (contest == null)
+                    return new ContestUpdate(); // Not started yet.
+            }
 
             var data = contest.GetData();
 
