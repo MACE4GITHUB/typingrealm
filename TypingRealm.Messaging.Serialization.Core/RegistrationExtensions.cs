ad55ee7553ee436602f888fda6e9504cb5fcef97
Ivan Zyranau <ewancoder@gmail.com>
2021-04-12 12:28:07 +0300

    Fix scoped dependency in singleton issue
    Scoped dependency cannot be instantiated within a Singleton. This is
    only used by client, but it's injected into server code as well (for
    now) so we need to register it as Singleton so it works.

--- TypingRealm.Messaging.Serialization.Core/RegistrationExtensions.cs
+++ TypingRealm.Messaging.Serialization.Core/RegistrationExtensions.cs
@@ -17,9 +17,9 @@ namespace TypingRealm.Messaging.Serialization
             // !!! but then we also need to split client and server versions of protobuf/signalr connection factories, so it's easier to just always register these types.
             services.AddTransient<IClientToServerMessageMetadataFactory, ClientToServerMessageMetadataFactory>();
 
-            // This is message idempotency Id. Scope this to the connection.
-            // TODO: Test that it works as expected.
-            services.AddScoped<IMessageIdFactory, MessageIdFactory>();
+            // This is used only from the client so no need to register it as Scoped.
+            // And it won't work as scoped because it is injected into singletone TCP/SignalR Server classes.
+            services.AddSingleton<IMessageIdFactory, MessageIdFactory>();
 
             return new MessageTypeCacheBuilder(services)
                 .AddMessageTypesFromAssembly(typeof(Disconnect).Assembly);
