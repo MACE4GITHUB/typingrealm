fc3482a5ab41f219085b99dfde8cbf04d8187b3c
Ivan Zyranau <ewancoder@gmail.com>
2021-07-25 02:10:18 +0300

    Add some nice descriptions to the two starting locations

--- TypingRealm.Data.Infrastructure/InMemoryLocationRepository.cs
+++ TypingRealm.Data.Infrastructure/InMemoryLocationRepository.cs
@@ -17,7 +17,25 @@ namespace TypingRealm.Data.Infrastructure
 
         public InMemoryLocationRepository()
         {
-            _cache.Add("1", new Location());
+            _cache.Add("1", new Location
+            {
+                LocationId = "1",
+                LocationEntrances = new List<string> { "2" },
+                CanHaveRopeWar = false,
+
+                Name = "Starting village",
+                Description = "This is a starting village. There's nothing to do here. Go to the training grounds to participate in rope wars."
+            });
+
+            _cache.Add("2", new Location
+            {
+                LocationId = "2",
+                LocationEntrances = new List<string> { "1" },
+                CanHaveRopeWar = true,
+
+                Name = "Training grounds",
+                Description = "Here you can train - have a rope war contest or something..."
+            });
         }
 
         public ValueTask<Location?> FindAsync(string locationId, CancellationToken cancellationToken)
