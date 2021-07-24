d17972f52976913688e61a749365d07ff6662f9e
Ivan Zyranau <ewancoder@gmail.com>
2021-07-24 07:00:00 +0300

    Add some basic data to the location (in Data context)

--- TypingRealm.Data.Resources/Location.cs
+++ TypingRealm.Data.Resources/Location.cs
@@ -1,6 +1,15 @@
-﻿namespace TypingRealm.Data.Resources
+﻿using System.Collections.Generic;
+
+namespace TypingRealm.Data.Resources
 {
+#pragma warning disable CS8618
     public class Location
     {
+        public string LocationId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool CanHaveRopeWar { get; set; }
+        public List<string> LocationEntrances { get; set; } = new List<string>();
     }
+#pragma warning restore CS8618
 }
