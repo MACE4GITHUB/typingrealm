bdb5e64e603a8db3f40fc929706eee030d8fb92e
Ivan Zyranau <ewancoder@gmail.com>
2021-11-21 01:48:03 +0300

    Improve report generator, show key pairs

--- TypingRealm.Typing/ITypingReportGenerator.cs
+++ TypingRealm.Typing/ITypingReportGenerator.cs
@@ -5,15 +5,19 @@ using System.Threading.Tasks;
 namespace TypingRealm.Typing
 {
     public sealed record TypingReport(
-        IDictionary<string, List<decimal>> KeyDelays,
-        IEnumerable<KeyValuePair<string, decimal>> averageKeyDelays,
-        IEnumerable<KeyValuePair<string, decimal>> maxKeyDelays,
-        IEnumerable<KeyValuePair<string, decimal>> minKeyDelays);
+        IDictionary<string, List<KeyDelayData>> KeyDelays,
+        IEnumerable<KeyValuePair<string, KeyDelayData>> averageKeyDelays,
+        IEnumerable<KeyValuePair<string, KeyDelayData>> maxKeyDelays,
+        IEnumerable<KeyValuePair<string, KeyDelayData>> minKeyDelays);
 
     public interface ITypingReportGenerator
     {
         ValueTask<TypingReport> GenerateReportAsync(string userId);
     }
+
+    public sealed record KeyDelayData(
+        decimal Delay,
+        string KeyPair);
 
     public sealed class TypingReportGenerator : ITypingReportGenerator
     {
@@ -26,38 +30,54 @@ namespace TypingRealm.Typing
 
         public async ValueTask<TypingReport> GenerateReportAsync(string userId)
         {
-            var keyDelays = new Dictionary<string, List<decimal>>();
+            var keyDelays = new Dictionary<string, List<KeyDelayData>>();
 
             await foreach (var userSession in _userSessionRepository.FindAllForUser(userId))
             {
                 foreach (var textTypingResult in userSession.GetTextTypingResults())
                 {
+                    var previousEvent = textTypingResult.Events.First();
                     foreach (var @event in textTypingResult.Events.Skip(1))
                     {
-                        if (!keyDelays.ContainsKey(@event.Key))
-                            keyDelays.Add(@event.Key, new List<decimal>());
+                        if (@event.KeyAction != KeyAction.Press)
+                            continue;
 
-                        keyDelays[@event.Key].Add(@event.Delay);
+                        if (!keyDelays.ContainsKey(@event.Key))
+                            keyDelays.Add(@event.Key, new List<KeyDelayData>());
+
+                        keyDelays[@event.Key].Add(new KeyDelayData(@event.AbsoluteDelay - previousEvent.AbsoluteDelay, $"{previousEvent.Key} -> {@event.Key}"));
+                        previousEvent = @event;
                     }
                 }
             }
 
+            foreach (var item in keyDelays)
+            {
+                var list = item.Value.ToList();
+
+                item.Value.Clear();
+                item.Value.AddRange(list.OrderBy(x => x.Delay));
+            }
+
             var averageKeyDelays = keyDelays.ToDictionary(
                 x => x.Key,
-                x => x.Value.Average())
-                .OrderBy(x => x.Value);
+                x => new KeyDelayData(x.Value.Average(y => y.Delay), ""))
+                .OrderBy(x => x.Value.Delay)
+                .ToList();
 
             var shortestKeyDelays = keyDelays.ToDictionary(
                 x => x.Key,
-                x => x.Value.Min())
-                .OrderBy(x => x.Value);
+                x => x.Value.OrderBy(y => y.Delay).First())
+                .OrderBy(x => x.Value.Delay)
+                .ToList();
 
             var longestKeyDelays = keyDelays.ToDictionary(
                 x => x.Key,
-                x => x.Value.Max())
-                .OrderBy(x => x.Value);
+                x => x.Value.OrderByDescending(y => y.Delay).First())
+                .OrderBy(x => x.Value.Delay)
+                .ToList();
 
-            return new TypingReport(keyDelays, averageKeyDelays, shortestKeyDelays, longestKeyDelays);
+            return new TypingReport(keyDelays, averageKeyDelays, longestKeyDelays, shortestKeyDelays);
         }
     }
 }
