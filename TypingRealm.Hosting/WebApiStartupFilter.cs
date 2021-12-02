d6bedf83fff6af4dd2607bd3cc9a9d998421572d
Ivan Zyranau <ewancoder@gmail.com>
2021-12-03 00:19:03 +0300

    Forward HTTP error status code throughout services call chain

--- TypingRealm.Hosting/WebApiStartupFilter.cs
+++ TypingRealm.Hosting/WebApiStartupFilter.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 
 namespace TypingRealm.Hosting
@@ -12,6 +16,21 @@ namespace TypingRealm.Hosting
         {
             return app =>
             {
+                // Forward HTTP errors throughout services call chain.
+                app.UseExceptionHandler(exceptionHandlerApp =>
+                {
+                    exceptionHandlerApp.Run(context =>
+                    {
+                        var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                        if (contextFeature?.Error is HttpRequestException exception)
+                        {
+                            context.Response.StatusCode = (int)(exception.StatusCode ?? HttpStatusCode.InternalServerError);
+                        }
+
+                        return Task.CompletedTask;
+                    });
+                });
+
                 app.UseRouting();
                 app.UseCors(CorsPolicyName);
                 app.UseAuthentication();
