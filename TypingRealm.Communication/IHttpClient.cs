ab762923a88b6400e9311dd0d1f502f5fbe4ccf4
Ivan Zyranau <ewancoder@gmail.com>
2021-12-03 00:03:47 +0300

    Tidy up http client extensions logic

--- TypingRealm.Communication/IHttpClient.cs
+++ TypingRealm.Communication/IHttpClient.cs
@@ -11,18 +11,35 @@ namespace TypingRealm.Communication
 {
     public static class HttpClientExtensions
     {
+        public static ValueTask<TResponse> SendMessageAsync<TResponse, TBody>(
+            this HttpClient httpClient,
+            HttpRequestMessage message,
+            string? accessToken,
+            TBody body,
+            CancellationToken cancellationToken)
+        {
+            SetMessageContent(message, body);
+            return SendMessageAsync<TResponse>(httpClient, message, accessToken, cancellationToken);
+        }
+
+        public static ValueTask SendMessageAsync<TBody>(
+            this HttpClient httpClient,
+            HttpRequestMessage message,
+            string? accessToken,
+            TBody body,
+            CancellationToken cancellationToken)
+        {
+            SetMessageContent(message, body);
+            return SendMessageAsync(httpClient, message, accessToken, cancellationToken);
+        }
+
         public static async ValueTask<TResponse> SendMessageAsync<TResponse>(
-            this HttpClient httpClient, HttpRequestMessage message, string? accessToken, CancellationToken cancellationToken)
+            this HttpClient httpClient,
+            HttpRequestMessage message,
+            string? accessToken,
+            CancellationToken cancellationToken)
         {
-            // TODO: Use serialization from Messaging.Serialization.Core, don't duplicate code.
-            var options = new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            };
-            options.Converters.Add(new JsonStringEnumConverter());
-
-            if (accessToken != null)
-                message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            SetAuthentication(message, accessToken);
 
             var response = await httpClient.SendAsync(message, cancellationToken)
                 .ConfigureAwait(false);
@@ -35,85 +52,51 @@ namespace TypingRealm.Communication
             if (typeof(TResponse).IsPrimitive || typeof(TResponse) == typeof(string))
                 return (TResponse)Convert.ChangeType(content, typeof(TResponse));
 
-            var deserialized = JsonSerializer.Deserialize<TResponse>(content, options);
+            var deserialized = JsonSerializer.Deserialize<TResponse>(content, CreateJsonSerializerOptions());
             if (deserialized == null)
                 throw new InvalidOperationException("Could not deserialize response from API.");
 
             return deserialized;
-        }
-
-        public static async ValueTask<TResponse> SendMessageAsync<TResponse, TBody>(
-            this HttpClient httpClient, HttpRequestMessage message, string? accessToken, TBody body, CancellationToken cancellationToken)
-        {
-            // TODO: Use serialization from Messaging.Serialization.Core, don't duplicate code.
-            var options = new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            };
-            options.Converters.Add(new JsonStringEnumConverter());
-
-            using var bodyStringContent = new StringContent(
-                JsonSerializer.Serialize(body, options: options),
-                Encoding.UTF8,
-                "application/json");
-            message.Content = bodyStringContent;
-
-            if (accessToken != null)
-                message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-
-            var response = await httpClient.SendAsync(message, cancellationToken)
-                .ConfigureAwait(false);
-
-            response.EnsureSuccessStatusCode();
-
-            var content = await response.Content.ReadAsStringAsync(cancellationToken)
-                .ConfigureAwait(false);
-
-            if (typeof(TResponse).IsPrimitive || typeof(TResponse) == typeof(string))
-                return (TResponse)Convert.ChangeType(content, typeof(TResponse));
-
-            var deserialized = JsonSerializer.Deserialize<TResponse>(content, options);
-            if (deserialized == null)
-                throw new InvalidOperationException("Could not deserialize response from API.");
-
-            return deserialized;
-        }
-
-        public static async ValueTask SendMessageAsync<TBody>(
-            this HttpClient httpClient, HttpRequestMessage message, string? accessToken, TBody body, CancellationToken cancellationToken)
-        {
-            // TODO: Use serialization from Messaging.Serialization.Core, don't duplicate code.
-            var options = new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            };
-            options.Converters.Add(new JsonStringEnumConverter());
-
-            using var bodyStringContent = new StringContent(
-                JsonSerializer.Serialize(body, options: options),
-                Encoding.UTF8,
-                "application/json");
-            message.Content = bodyStringContent;
-
-            if (accessToken != null)
-                message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-
-            var response = await httpClient.SendAsync(message, cancellationToken)
-                .ConfigureAwait(false);
-
-            response.EnsureSuccessStatusCode();
         }
 
         public static async ValueTask SendMessageAsync(
-            this HttpClient httpClient, HttpRequestMessage message, string? accessToken, CancellationToken cancellationToken)
+            this HttpClient httpClient,
+            HttpRequestMessage message,
+            string? accessToken,
+            CancellationToken cancellationToken)
         {
-            if (accessToken != null)
-                message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            SetAuthentication(message, accessToken);
 
             var response = await httpClient.SendAsync(message, cancellationToken)
                 .ConfigureAwait(false);
 
             response.EnsureSuccessStatusCode();
+        }
+
+        private static void SetAuthentication(HttpRequestMessage message, string? accessToken)
+        {
+            if (accessToken != null)
+                message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        }
+
+        private static void SetMessageContent<TContent>(HttpRequestMessage message, TContent body)
+        {
+            using var bodyStringContent = new StringContent(
+                JsonSerializer.Serialize(body, options: CreateJsonSerializerOptions()),
+                Encoding.UTF8,
+                "application/json");
+            message.Content = bodyStringContent;
+        }
+
+        private static JsonSerializerOptions CreateJsonSerializerOptions()
+        {
+            // TODO: Use serialization from Messaging.Serialization.Core, don't duplicate code.
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            options.Converters.Add(new JsonStringEnumConverter());
+            return options;
         }
     }
 }
