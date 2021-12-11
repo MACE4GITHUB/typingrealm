24bee2783e2fd3f68e4f16a23ce978f9ba195d95
Ivan Zyranau <ewancoder@gmail.com>
2021-12-11 03:29:36 +0300

    Build Local profile after Dev so that whole script doesn't fail

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ if [ "$1" == "prod" ]; then
     docker-compose -p tyr -f ./docker-compose.prod.yml up -d --build
 elif [ "$1" == "all" ]; then
     docker-compose -p tyr -f ./docker-compose.prod.yml up -d --build
-    docker-compose -p local-tyr -f ./docker-compose.local.yml up -d --build
     docker-compose -p dev-tyr -f ./docker-compose.dev.yml up -d --build
+    docker-compose -p local-tyr -f ./docker-compose.local.yml up -d --build
 else
     docker-compose -p $1-tyr -f ./docker-compose.$1.yml up -d --build
 fi
