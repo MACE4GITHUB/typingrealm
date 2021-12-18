0826c59600fd039bc187457013e46dfea8a830b8
Ivan Zyranau <ewancoder@gmail.com>
2021-12-19 02:46:41 +0300

    Move Local authentication controller from Program.cs file

---
+++ TypingRealm.IdentityServer.Host/LocalAuthenticationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using TypingRealm.Authentication.OAuth;
+
+namespace TypingRealm.IdentityServer.Host
+{
+    [AllowAnonymous]
+    [Route("api/local")]
+    public sealed class LocalAuthenticationController : ControllerBase
+    {
+        private readonly IHostEnvironment _environment;
+
+        public LocalAuthenticationController(IHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        [HttpPost]
+        [Route("user-token")]
+        public ActionResult GenerateToken(string sub)
+        {
+            if (!_environment.IsDevelopment())
+                return NotFound();
+
+            if (string.IsNullOrWhiteSpace(sub))
+                return BadRequest("Sub (user ID) should not be empty.");
+
+            return Ok(new
+            {
+                access_token = LocalAuthentication.GenerateProfileAccessToken($"local-auth_{sub}")
+            });
+        }
+    }
+}
--- TypingRealm.IdentityServer.Host/Startup.cs
+++ TypingRealm.IdentityServer.Host/Startup.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using TypingRealm.Authentication.OAuth;
 
 namespace TypingRealm.IdentityServer.Host
 {
@@ -28,34 +24,6 @@ namespace TypingRealm.IdentityServer.Host
             app.UseRouting();
             app.UseEndpoints(e => e.MapControllers());
             app.UseIdentityServer();
-        }
-    }
-
-    [AllowAnonymous]
-    [Route("api/local")]
-    public sealed class LocalAuthenticationController : ControllerBase
-    {
-        private readonly IHostEnvironment _environment;
-
-        public LocalAuthenticationController(IHostEnvironment environment)
-        {
-            _environment = environment;
-        }
-
-        [HttpPost]
-        [Route("user-token")]
-        public ActionResult GenerateToken(string sub)
-        {
-            if (!_environment.IsDevelopment())
-                return NotFound();
-
-            if (string.IsNullOrWhiteSpace(sub))
-                return BadRequest("Sub (user ID) should not be empty.");
-
-            return Ok(new
-            {
-                access_token = LocalAuthentication.GenerateProfileAccessToken($"local-auth_{sub}")
-            });
         }
     }
 }
