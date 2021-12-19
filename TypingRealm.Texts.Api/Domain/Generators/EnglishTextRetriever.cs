18429c521f5acc99f1a665a7e10240c23bd4bb10
Ivan Zyranau <ewancoder@gmail.com>
2021-12-20 00:02:43 +0300

    Accept only texts with valid characters

--- TypingRealm.Texts.Api/Domain/Generators/EnglishTextRetriever.cs
+++ TypingRealm.Texts.Api/Domain/Generators/EnglishTextRetriever.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -9,7 +10,8 @@ namespace TypingRealm.Texts.Generators
     {
         private sealed record QuotableResponse(string content);
 
-        public string Language => "en";
+        private const int RetriesCount = 10;
+        private static readonly string _allowedLetters = "'\",<.>pPyYfFgGcCrRlL/?=+\\|aAoOeEuUiIdDhHtTnNsS-_;:qQjJkKxXbBmMwWvVzZ 1!2@3#4$5%6^7&8*9(0)[{]}`~";
         private readonly IHttpClientFactory _httpClientFactory;
 
         public EnglishTextRetriever(IHttpClientFactory httpClientFactory)
@@ -17,23 +19,36 @@ namespace TypingRealm.Texts.Generators
             _httpClientFactory = httpClientFactory;
         }
 
+        public string Language => "en";
+
         public async ValueTask<string> RetrieveTextAsync()
         {
             var httpClient = _httpClientFactory.CreateClient();
 
-            using var response = await httpClient.GetAsync("https://api.quotable.io/random")
-                .ConfigureAwait(false);
+            var count = 0; // Avoid endless loops.
+            while (count < RetriesCount)
+            {
+                count++;
 
-            response.EnsureSuccessStatusCode();
+                using var response = await httpClient.GetAsync("https://api.quotable.io/random")
+                    .ConfigureAwait(false);
 
-            var content = await response.Content.ReadAsStringAsync()
-                .ConfigureAwait(false);
+                response.EnsureSuccessStatusCode();
 
-            var textValue = JsonSerializer.Deserialize<QuotableResponse>(content)?.content;
-            if (textValue == null)
-                throw new InvalidOperationException("Error when trying to get response from quotable API: invalid content.");
+                var content = await response.Content.ReadAsStringAsync()
+                    .ConfigureAwait(false);
 
-            return textValue;
+                var textValue = JsonSerializer.Deserialize<QuotableResponse>(content)?.content;
+                if (textValue == null)
+                    throw new InvalidOperationException("Error when trying to get response from quotable API: invalid content.");
+
+                if (!textValue.All(character => _allowedLetters.Contains(character)))
+                    continue;
+
+                return textValue;
+            }
+
+            throw new InvalidOperationException("Could not get text within allowed characters limit.");
         }
     }
 }
--- TypingRealm.Texts.Api/Domain/Generators/RussianTextRetriever.cs
+++ TypingRealm.Texts.Api/Domain/Generators/RussianTextRetriever.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -6,7 +7,8 @@ namespace TypingRealm.Texts.Generators
 {
     public sealed class RussianTextRetriever : ITextRetriever
     {
-        public string Language => "ru";
+        private const int RetriesCount = 10;
+        private static readonly string _allowedLetters = "'\",<.>/?=+\\|-_;: 1!2@3#4$5%6^7&8*9(0)[{]}`~йЙцЦуУкКеЕнНгГшШщЩзЗхХъЪфФыЫвВаАпПрРоОлЛдДжЖэЭяЯчЧсСмМиИтТьЬбБюЮёЁ";
         private readonly IHttpClientFactory _httpClientFactory;
 
         public RussianTextRetriever(IHttpClientFactory httpClientFactory)
@@ -14,24 +16,35 @@ namespace TypingRealm.Texts.Generators
             _httpClientFactory = httpClientFactory;
         }
 
+        public string Language => "ru";
+
         public async ValueTask<string> RetrieveTextAsync()
         {
             var httpClient = _httpClientFactory.CreateClient();
 
-            using var response = await httpClient.GetAsync("https://fish-text.ru/get?format=html")
-                .ConfigureAwait(false);
+            var count = 0; // Avoid endless loops.
+            while (count < RetriesCount)
+            {
+                using var response = await httpClient.GetAsync("https://fish-text.ru/get?format=html")
+                    .ConfigureAwait(false);
 
-            response.EnsureSuccessStatusCode();
+                response.EnsureSuccessStatusCode();
 
-            var content = await response.Content.ReadAsStringAsync()
-                .ConfigureAwait(false);
+                var content = await response.Content.ReadAsStringAsync()
+                    .ConfigureAwait(false);
 
-            var value = content.Replace("<p>", string.Empty).Replace("</p>", string.Empty);
+                var value = content.Replace("<p>", string.Empty).Replace("</p>", string.Empty);
 
-            if (string.IsNullOrWhiteSpace(value))
-                throw new InvalidOperationException("Text API returned empty text.");
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new InvalidOperationException("Text API returned empty text.");
 
-            return value;
+                if (!value.All(character => _allowedLetters.Contains(character)))
+                    continue;
+
+                return value;
+            }
+
+            throw new InvalidOperationException("Could not get text within allowed characters limit.");
         }
     }
 }
