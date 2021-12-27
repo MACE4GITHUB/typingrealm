7dce40fca80c2136287ee6942fd2831c52f45066
Ivan Zyranau <ewancoder@gmail.com>
2021-12-27 17:20:17 +0300

    Add possibility to generate texts offline, fix an issue in the process
    Fixes an issue that Cached provider was wrapped around Cached provider.

---
+++ TypingRealm.Texts.Api/Domain/Generators/OfflineTextRetriever.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace TypingRealm.Texts.Generators;
+
+public sealed class OfflineTextRetriever : ITextRetriever
+{
+    private readonly List<string> _texts;
+
+    public OfflineTextRetriever(string language, IEnumerable<string> texts)
+    {
+        _texts = texts.ToList();
+        Language = language;
+    }
+
+    public string Language { get; }
+
+    public ValueTask<string> RetrieveTextAsync()
+    {
+        var index = RandomNumberGenerator.GetInt32(0, _texts.Count);
+
+        return new ValueTask<string>(_texts[index]);
+    }
+}
--- TypingRealm.Texts.Api/Domain/RegistrationExtensions.cs
+++ TypingRealm.Texts.Api/Domain/RegistrationExtensions.cs
@@ -16,15 +16,34 @@ public static class RegistrationExtensions
 
         services.AddTransient<ITextGenerator, TextGenerator>();
 
-        services.AddTransient<EnglishTextRetriever>();
-        services.AddSingleton<ITextRetriever>(provider => new CachedTextRetriever(
-            provider.GetRequiredService<EnglishTextRetriever>(),
-            provider.GetRequiredService<IServiceCacheProvider>()));
+        if (DebugHelpers.UseInfrastructure)
+        {
+            services.AddTransient<EnglishTextRetriever>();
+            services.AddSingleton<ITextRetriever>(provider => new CachedTextRetriever(
+                provider.GetRequiredService<EnglishTextRetriever>(),
+                provider.GetRequiredService<IServiceCacheProvider>()));
 
-        services.AddTransient<RussianTextRetriever>();
-        services.AddSingleton<ITextRetriever>(provider => new CachedTextRetriever(
-            provider.GetRequiredService<RussianTextRetriever>(),
-            provider.GetRequiredService<IServiceCacheProvider>()));
+            services.AddTransient<RussianTextRetriever>();
+            services.AddSingleton<ITextRetriever>(provider => new CachedTextRetriever(
+                provider.GetRequiredService<RussianTextRetriever>(),
+                provider.GetRequiredService<IServiceCacheProvider>()));
+        }
+        else
+        {
+            services.AddSingleton<ITextRetriever>(new OfflineTextRetriever("en", new[]
+            {
+                "Love is rarer than genius itself. And friendship is rarer than love.",
+                "Don't leave a stone unturned. It's always something, to know you have done the most you could.",
+                "Never, never, never give up."
+            }));
+
+            services.AddSingleton<ITextRetriever>(new OfflineTextRetriever("ru", new[]
+            {
+                "Высокий уровень вовлечения представителей целевой аудитории является четким доказательством простого факта: дальнейшее развитие различных форм деятельности является качественно новой ступенью своевременного выполнения сверхзадачи!",
+                "Есть над чем задуматься: сделанные на базе интернет-аналитики выводы, превозмогая сложившуюся непростую экономическую ситуацию, функционально разнесены на независимые элементы.",
+                "Кстати, базовые сценарии поведения пользователей призывают нас к новым свершениям, которые, в свою очередь, должны быть объединены в целые кластеры себе подобных. В частности, новая модель организационной деятельности, а также свежий взгляд на привычные вещи - безусловно открывает новые горизонты для укрепления моральных ценностей."
+            }));
+        }
 
         services.AddTransient<TextRetrieverResolver>(provider => language =>
         {
@@ -34,9 +53,7 @@ public static class RegistrationExtensions
             if (retriever == null)
                 throw new NotSupportedException($"Text generator for language {language} is not supported.");
 
-            return new CachedTextRetriever(
-                retriever,
-                provider.GetRequiredService<IServiceCacheProvider>());
+            return retriever;
         });
 
         return services;
