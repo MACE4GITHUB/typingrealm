8024fc37176514a1fb8f84da373903bd766102b5
Ivan Zyranau <ewancoder@gmail.com>
2022-01-08 01:57:38 +0300

    Generate proper environment files for debug environment
    And also fix the issue with wrong service identity for port generation.

--- Utilities/TypingRealm.DeploymentHelper/Entities.cs
+++ Utilities/TypingRealm.DeploymentHelper/Entities.cs
@@ -62,7 +62,7 @@ namespace TypingRealm.DeploymentHelper
             var serviceInfos = Services
                 .Where(service => service.Envs == null || service.Envs.Contains(environment.Value))
                 .OrderBy(service => service.ServiceName)
-                .SelectMany((service, index) => GetDockerServices(service, environment, index))
+                .SelectMany(service => GetDockerServices(service, environment))
                 .ToList();
 
             // TODO: Add Caddy for some envs like "prod/host" and "local".
@@ -70,7 +70,7 @@ namespace TypingRealm.DeploymentHelper
             return serviceInfos;
         }
 
-        private IEnumerable<ServiceInformation> GetDockerServices(Service service, Environment environment, int index)
+        private IEnumerable<ServiceInformation> GetDockerServices(Service service, Environment environment)
         {
             // TODO: Move to common place and reuse in GetNetworks.
             var dockerName = $"{environment.EnvironmentPrefix}{ProjectName}-{service.ServiceName}";
@@ -96,7 +96,7 @@ namespace TypingRealm.DeploymentHelper
                         GetEnvFiles(service, environment),
                         environment.HideInfrastructurePorts
                             ? Enumerable.Empty<string>()
-                            : new[] { GetInfrastructurePort(5432, environment, index) },
+                            : new[] { GetInfrastructurePort(5432, environment, service) },
                         GetPostgresVolumes(service, environment));
                 }
 
@@ -110,7 +110,7 @@ namespace TypingRealm.DeploymentHelper
                         GetEnvFiles(service, environment),
                         environment.HideInfrastructurePorts
                             ? Enumerable.Empty<string>()
-                            : new[] { GetInfrastructurePort(6379, environment, index) },
+                            : new[] { GetInfrastructurePort(6379, environment, service) },
                         GetRedisVolumes(service, environment));
                 }
             }
@@ -145,9 +145,9 @@ namespace TypingRealm.DeploymentHelper
             return Enumerable.Empty<string>();
         }
 
-        private string GetInfrastructurePort(int infrastructurePort, Environment environment, int index)
+        public static string GetInfrastructurePort(int infrastructurePort, Environment environment, Service service)
         {
-            var portPrefix = $"{environment.PortPrefix}{index.ToString("D2")}";
+            var portPrefix = $"{environment.PortPrefix}{service.Index.ToString("D2")}";
             return $"{portPrefix}{infrastructurePort.ToString("D5").Substring(3, 2)}:{infrastructurePort}";
         }
 
@@ -221,8 +221,8 @@ namespace TypingRealm.DeploymentHelper
         public bool RestartUnlessStopped => Value != "debug";
         public bool DeployInfrastructure => Value != "debug";
         public bool IsDevelopmentEnv => Value != "prod" && Value != "strict-prod";
-
         public bool GenerateEnvFiles => Value != "strict-prod";
+        public bool IsDebug => Value == "debug";
     }
 
     // TODO: Handle "Caddy" service separately (hardcode?).
@@ -251,18 +251,40 @@ namespace TypingRealm.DeploymentHelper
                     continue;
                 }
 
-                envVars.Add(new EnvVariable($"{service.ServiceName.ToUpperInvariant()}_URL", $"http://{environment.EnvironmentPrefix}{DeploymentData.ProjectName}-{service.ServiceName}"));
+                var serviceAddress = environment.IsDebug
+                    ? $"http://host.docker.internal:{service.Port}"
+                    : $"http://{environment.EnvironmentPrefix}{DeploymentData.ProjectName}-{service.ServiceName}";
+
+                envVars.Add(new EnvVariable($"{service.ServiceName.ToUpperInvariant()}_URL", serviceAddress));
 
                 if (service.CacheType == CacheType.Redis)
                 {
-                    serviceEnvVars[service.ServiceName].EnvVars.Add(new EnvVariable("ConnectionStrings__ServiceCacheConnection", $"{environment.EnvironmentPrefix}{DeploymentData.ProjectName}-{service.ServiceName}-redis:6379"));
+                    if (environment.IsDebug)
+                    {
+                        var devRedisPort = DeploymentData.GetInfrastructurePort(6379, new Environment("dev"), service).Split(":")[0];
+                        serviceEnvVars[service.ServiceName].EnvVars.Add(new EnvVariable("ConnectionStrings__ServiceCacheConnection", $"host.docker.internal:{devRedisPort}"));
+                    }
+                    else
+                    {
+                        serviceEnvVars[service.ServiceName].EnvVars.Add(new EnvVariable("ConnectionStrings__ServiceCacheConnection", $"{environment.EnvironmentPrefix}{DeploymentData.ProjectName}-{service.ServiceName}-redis:6379"));
+                    }
                 }
 
                 if (service.DatabaseType == DatabaseType.Postgres)
                 {
-                    var connectionString = $"Server={environment.EnvironmentPrefix}{DeploymentData.ProjectName}-{service.ServiceName}-postgres; Port=5432; User Id=postgres; Password=admin; Database=db";
-                    serviceEnvVars[service.ServiceName].EnvVars.Add(new EnvVariable("ConnectionStrings__DataConnection", connectionString));
                     serviceEnvVars[service.ServiceName].EnvVars.Add(new EnvVariable("POSTGRES_PASSWORD", "admin"));
+                    if (environment.IsDebug)
+                    {
+                        var server = $"host.docker.internal";
+                        var devPostgresPort = DeploymentData.GetInfrastructurePort(5432, new Environment("dev"), service).Split(":")[0];
+                        var connectionString = $"Server={server}; Port={devPostgresPort}; User Id=postgres; Password=admin; Database=db";
+                        serviceEnvVars[service.ServiceName].EnvVars.Add(new EnvVariable("ConnectionStrings__DataConnection", connectionString));
+                    }
+                    else
+                    {
+                        var connectionString = $"Server={environment.EnvironmentPrefix}{DeploymentData.ProjectName}-{service.ServiceName}-postgres; Port=5432; User Id=postgres; Password=admin; Database=db";
+                        serviceEnvVars[service.ServiceName].EnvVars.Add(new EnvVariable("ConnectionStrings__DataConnection", connectionString));
+                    }
                 }
             }
 
