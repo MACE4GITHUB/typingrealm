d52d21eb9410ffd12c6e27bff7bf3d351ed4f868
Ivan Zyranau <ewancoder@gmail.com>
2022-01-08 02:17:15 +0300

    Add .deployment prefix to all generated env files

--- Utilities/TypingRealm.DeploymentHelper/Entities.cs
+++ Utilities/TypingRealm.DeploymentHelper/Entities.cs
@@ -181,7 +181,7 @@ namespace TypingRealm.DeploymentHelper
             if (value == "prod" || value == "strict-prod")
             {
                 EnvironmentPrefix = string.Empty;
-                EnvironmentFileName = ".env.prod";
+                EnvironmentFileName = ".deployment/.env.prod";
                 VolumeFolderName = "prod";
 
                 if (value == "prod")
@@ -195,7 +195,7 @@ namespace TypingRealm.DeploymentHelper
             if (value == "dev" || value == "local" || value == "debug")
             {
                 EnvironmentPrefix = $"{value}-";
-                EnvironmentFileName = $".env.{value}";
+                EnvironmentFileName = $".deployment/.env.{value}";
                 VolumeFolderName = value;
 
                 if (value == "dev")
