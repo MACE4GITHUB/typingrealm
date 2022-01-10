426573cd4a986909a83bd63bc48a541b00525193
Ivan Zyranau <ewancoder@gmail.com>
2022-01-10 03:53:51 +0300

    Improve deployment 'all' script: deploy only prod and dev, and in parallel

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,8 @@ set -Eeuo pipefail
 if [ "$1" == "prod" ]; then
     docker-compose -p tyr -f ./docker-compose.prod.yml up -d --build
 elif [ "$1" == "all" ]; then
-    docker-compose -p tyr -f ./docker-compose.prod.yml up -d --build
-    docker-compose -p dev-tyr -f ./docker-compose.dev.yml up -d --build
-    docker-compose -p local-tyr -f ./docker-compose.local.yml up -d --build
+    docker-compose -p tyr -f ./docker-compose.prod.yml up -d --build &
+    docker-compose -p dev-tyr -f ./docker-compose.dev.yml up -d --build &
 elif [ "$1" == "down" ]; then
     docker-compose -p local-tyr down
     docker-compose -p dev-tyr down
@@ -15,4 +14,3 @@ elif [ "$1" == "down" ]; then
 else
     docker-compose -p $1-tyr -f ./docker-compose.$1.yml up -d --build
 fi
-
