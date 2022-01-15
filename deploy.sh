0cb35ffcd8d8e746087c31262af35094042816d8
Ivan Zyranau <ewancoder@gmail.com>
2022-01-16 00:03:12 +0300

    Deploy global infrastructure separately, do not expose it to public network

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,11 @@ set -Eeuo pipefail
 
 if [ "$1" == "prod" ]; then
     docker-compose -p tyr -f ./docker-compose.prod.yml up -d --build
+elif [ "$1" == "services" ]; then
+    docker-compose -p tyr -f ./docker-compose.prod.yml up -d --build &
+    docker-compose -p dev-tyr -f ./docker-compose.dev.yml up -d --build &
 elif [ "$1" == "all" ]; then
+    docker-compose -p infra-tyr -f ./docker-compose.infra.yml up -d --build
     docker-compose -p tyr -f ./docker-compose.prod.yml up -d --build &
     docker-compose -p dev-tyr -f ./docker-compose.dev.yml up -d --build &
 elif [ "$1" == "down" ]; then
