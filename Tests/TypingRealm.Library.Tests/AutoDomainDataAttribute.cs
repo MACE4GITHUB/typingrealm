bf401c4e169ef1407e5a34754919a17d1f36d15f
Ivan Zyranau <ewancoder@gmail.com>
2022-01-20 02:24:15 +0300

    Move common domain test logic from Library to TextProcessing

--- Tests/TypingRealm.Library.Tests/AutoDomainDataAttribute.cs
+++ Tests/TypingRealm.Library.Tests/AutoDomainDataAttribute.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Reflection;
 using AutoFixture;
 using AutoFixture.Dsl;
-using AutoFixture.Kernel;
 using AutoFixture.Xunit2;
-using TypingRealm.Common;
 using TypingRealm.Library.Books;
 using TypingRealm.Testing;
-using TypingRealm.TextProcessing;
 
 namespace TypingRealm.Library.Tests
 {
@@ -18,6 +14,7 @@ namespace TypingRealm.Library.Tests
         public static IFixture CreateFixture()
         {
             return AutoMoqDataAttribute.CreateFixture()
+                .Customize(new TextProcessing.Tests.DomainCustomization())
                 .Customize(new DomainCustomization());
         }
     }
@@ -26,32 +23,7 @@ namespace TypingRealm.Library.Tests
     {
         public void Customize(IFixture fixture)
         {
-            fixture.Customizations.Add(new MaxLengthBuilder());
-            fixture.Register(() => TextConstants.EnglishLanguage);
             fixture.Register(() => Book.FromState(fixture.Create<Book.State>()));
-        }
-    }
-
-    // TODO: Move this builder to common testing, it works with any Primitives.
-    public class MaxLengthBuilder : ISpecimenBuilder
-    {
-        public object Create(object request, ISpecimenContext context)
-        {
-            if (request is ParameterInfo pi
-                && pi.ParameterType == typeof(string)
-                && pi.Member.DeclaringType != null
-                && pi.Member.DeclaringType.IsSubclassOf(typeof(Primitive<string>))
-                && pi.Member.DeclaringType.GetField("MaxLength") != null)
-            {
-                var maxLength = Convert.ToInt32(pi.Member.DeclaringType.GetField("MaxLength")!.GetValue(null));
-
-                var value = context.Create<string>();
-                var length = Math.Min(maxLength, value.Length);
-
-                return value[..length];
-            }
-
-            return new NoSpecimen();
         }
     }
 
