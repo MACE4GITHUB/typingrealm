5c71f4064356744a19ae0a91d24b26c30a150a5c
Ivan Zyranau <ewancoder@gmail.com>
2022-01-20 02:15:33 +0300

    Add TextProcessing unit testing project, cover Language by tests

---
+++ Tests/TypingRealm.TextProcessing.Tests/LanguageTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+
+namespace TypingRealm.TextProcessing.Tests
+{
+    public class LanguageTests
+    {
+        [Fact]
+        public void ShouldSupportAllSupportedLanguages()
+        {
+            foreach (var languageValue in TextConstants.SupportedLanguageValues)
+            {
+                var sut = new Language(languageValue);
+                Assert.Equal(languageValue, sut.Value);
+            }
+        }
+
+        [Theory]
+        [InlineData("fr")]
+        [InlineData("some")]
+        [InlineData("language")]
+        [InlineData("")]
+        public void ShouldNotSupportAnyWrongValues(string value)
+        {
+            Assert.Throws<ArgumentException>(() => new Language(value));
+        }
+
+        [Fact]
+        public void ShouldThrow_WhenNullIsPassed()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Language(null!));
+        }
+    }
+}
