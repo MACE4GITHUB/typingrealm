da162ad98a25d7963b53a5d5c378ba693676e68a
Ivan Zyranau <ewancoder@gmail.com>
2022-01-22 02:01:44 +0300

    Move TextProcessorExtensions tests to a separate file

---
+++ Tests/TypingRealm.TextProcessing.Tests/TextProcessorExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using AutoFixture;
+using Xunit;
+
+namespace TypingRealm.TextProcessing.Tests;
+
+public class TextProcessorExtensionsTests : TextProcessingTestsBase
+{
+    private readonly TextProcessor _sut;
+
+    public TextProcessorExtensionsTests()
+    {
+        _sut = Fixture.Create<TextProcessor>();
+    }
+
+    [Fact]
+    public void GetSentences_ShouldGetOnlySentencesWithAllowedCharacters()
+    {
+        var text = "abcd bc. bacd? abcd? bcde";
+
+        var languageInfo = Create<LanguageInformation>(
+            new LanguageInformationBuilder("Aabcd ?."));
+
+        var sentences = _sut.GetSentencesEnumerable(text, languageInfo)
+            .ToList();
+
+        Assert.Equal("Abcd bc.", sentences[0]);
+        Assert.Equal("Abcd?", sentences[1]);
+        Assert.Equal(2, sentences.Count);
+    }
+}
--- Tests/TypingRealm.TextProcessing.Tests/TextProcessorTests.cs
+++ Tests/TypingRealm.TextProcessing.Tests/TextProcessorTests.cs
@@ -6,32 +6,6 @@ using Xunit;
 
 namespace TypingRealm.TextProcessing.Tests;
 
-public class TextProcessorExtensionsTests : TextProcessingTestsBase
-{
-    private readonly TextProcessor _sut;
-
-    public TextProcessorExtensionsTests()
-    {
-        _sut = Fixture.Create<TextProcessor>();
-    }
-
-    [Fact]
-    public void GetSentences_ShouldGetOnlySentencesWithAllowedCharacters()
-    {
-        var text = "abcd bc. bacd? abcd? bcde";
-
-        var languageInfo = Create<LanguageInformation>(
-            new LanguageInformationBuilder("Aabcd ?."));
-
-        var sentences = _sut.GetSentencesEnumerable(text, languageInfo)
-            .ToList();
-
-        Assert.Equal("Abcd bc.", sentences[0]);
-        Assert.Equal("Abcd?", sentences[1]);
-        Assert.Equal(2, sentences.Count);
-    }
-}
-
 public class TextProcessorTests : TextProcessingTestsBase
 {
     private readonly TextProcessor _sut;
