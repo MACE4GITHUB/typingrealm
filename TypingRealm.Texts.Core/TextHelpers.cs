98327115b0d88a1f7e3c50e7a6a1735202dee1c9
Ivan Zyranau <ewancoder@gmail.com>
2022-01-23 03:21:56 +0300

    Remove unused class / file

--- TypingRealm.Texts.Core/TextHelpers.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TypingRealm.Texts;
-
-public static class TextHelpers
-{
-    // TODO: Consider moving this constant to Library context.
-    public static readonly int MinSentenceLength = 8;
-}
