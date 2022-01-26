8447fa018209091e279aaf3b0da4ae703fc68c84
Ivan Zyranau <ewancoder@gmail.com>
2022-01-27 00:23:35 +0300

    Add unit tests for Validation.ValidateIn method

--- Tests/TypingRealm.Common.Tests/ValidationTests.cs
+++ Tests/TypingRealm.Common.Tests/ValidationTests.cs
@@ -11,7 +11,8 @@ public class ValidationTests : TestsBase
     [InlineData("", 1, 4)]
     [InlineData("value", 6, 8)]
     [InlineData("value", 6, 6)]
-    public void ValidateLength_Error(string value, int minLength, int maxLength)
+    public void ValidateLength_ShouldThrowWhenValueIsOutsideOfRange(
+        string value, int minLength, int maxLength)
     {
         Assert.Throws<ArgumentException>(
             () => Validation.ValidateLength(value, minLength, maxLength));
@@ -24,8 +25,29 @@ public class ValidationTests : TestsBase
     [InlineData("", 0, 5)]
     [InlineData("", 0, 0)]
     [InlineData("value", 5, 5)]
-    public void ValidateLength_Successful(string value, int minLength, int maxLength)
+    public void ValidateLength_ShouldNotThrowWhenValueIsWithinRange(
+        string value, int minLength, int maxLength)
     {
         Validation.ValidateLength(value, minLength, maxLength);
+    }
+
+    [Theory]
+    [InlineData("value", new[] { "", "val1", "Value", "value " })]
+    [InlineData("", new[] { "val1", "Value", "value " })]
+    [InlineData(" value", new[] { "val1", "Value", "value " })]
+    public void ValidateIn_ShouldThrowWhenValueIsNotInRange(
+        string value, string[] validValues)
+    {
+        Assert.Throws<ArgumentException>(
+            () => Validation.ValidateIn(value, validValues));
+    }
+
+    [Theory]
+    [InlineData("value", new[] { "", "value", "another" })]
+    [InlineData("", new[] { "", "value", "another" })]
+    public void ValidateIn_ShouldNotThrowWhenValueIsInRange(
+        string value, string[] validValues)
+    {
+        Validation.ValidateIn(value, validValues);
     }
 }
