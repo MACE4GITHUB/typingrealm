0dadbf3d6da84c3c29815b7ca3489a97ba8b39e1
Ivan Zyranau <ewancoder@gmail.com>
2022-01-27 23:18:58 +0300

    Add documentation to common Validation methods

--- TypingRealm.Common/Validation.cs
+++ TypingRealm.Common/Validation.cs
@@ -6,12 +6,22 @@ namespace TypingRealm;
 
 public static class Validation
 {
+    /// <summary>
+    /// Throws <see cref="ArgumentException"/> when value has length less than
+    /// specified from characters, or more than specified to characters.
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
     public static void ValidateLength(string value, int from, int to)
     {
         if (value.Length < from || value.Length > to)
             throw new ArgumentException($"Value length should be between {from} and {to}.");
     }
 
+    /// <summary>
+    /// Throws <see cref="ArgumentException"/> when supplied valid values
+    /// collection does not contain the value.
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
     public static void ValidateIn(string value, IEnumerable<string> validValues)
     {
         if (!validValues.Any(x => value == x))
