5838761efe782a560bf980ff8ff2700b2db9b791
Ivan Zyranau <ewancoder@gmail.com>
2022-02-01 18:01:07 +0300

    Don't deploy multiple envs in parallel
    Parallel deployment only slows things down, because we need to build
    containers only once. Second deployment would be lightning-fast.

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,19 @@ set -Eeuo pipefail
 if [ "$1" == "prod" ]; then
     docker-compose -p tyr -f ./docker-compose.prod.yml up -d --build
 elif [ "$1" == "services" ]; then
-    docker-compose -p tyr -f ./docker-compose.prod.yml up -d --build &
-    docker-compose -p dev-tyr -f ./docker-compose.dev.yml up -d --build &
+    docker-compose -p tyr -f ./docker-compose.prod.yml up -d --build
+    docker-compose -p dev-tyr -f ./docker-compose.dev.yml up -d --build
 elif [ "$1" == "all" ]; then
     docker-compose -p infra-tyr -f ./docker-compose.infra.yml up -d --build
-    docker-compose -p tyr -f ./docker-compose.prod.yml up -d --build &
-    docker-compose -p dev-tyr -f ./docker-compose.dev.yml up -d --build &
+    docker-compose -p tyr -f ./docker-compose.prod.yml up -d --build
+    docker-compose -p dev-tyr -f ./docker-compose.dev.yml up -d --build
 elif [ "$1" == "down-all" ]; then
     docker-compose -p dev-tyr -f ./docker-compose.dev.yml down
     docker-compose -p tyr -f ./docker-compose.prod.yml down
     docker-compose -p infra-tyr -f ./docker-compose.infra.yml down
 elif [ "$1" == "down-services" ]; then
-    docker-compose -p dev-tyr -f ./docker-compose.dev.yml down &
-    docker-compose -p tyr -f ./docker-compose.prod.yml down &
+    docker-compose -p dev-tyr -f ./docker-compose.dev.yml down
+    docker-compose -p tyr -f ./docker-compose.prod.yml down
 elif [ "$1" == "down-dev" ]; then
     docker-compose -p dev-tyr -f ./docker-compose.dev.yml down
 elif [ "$1" == "down-prod" ]; then
