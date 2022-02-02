3e31656905d8da5bf6824bed21aa488a90991b15
Ivan Zyranau <ewancoder@gmail.com>
2022-02-03 00:26:49 +0300

    Add documentation for IMessageIdFactory

--- TypingRealm.Messaging.Core/IMessageIdFactory.cs
+++ TypingRealm.Messaging.Core/IMessageIdFactory.cs
@@ -1,5 +1,9 @@
 ﻿namespace TypingRealm.Messaging;
 
+/// <summary>
+/// Used to create a unique message identifier before sending it to the second party.
+/// For now it's only used by client side, but it can be a universal interface.
+/// </summary>
 public interface IMessageIdFactory
 {
     string CreateMessageId();
