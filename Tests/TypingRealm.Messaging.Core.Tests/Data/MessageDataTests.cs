5f07d0621f6385fd3d595a3052dac7cd1b8faebd
Ivan Zyranau <ewancoder@gmail.com>
2022-02-09 20:12:39 +0300

    Add MessageData tests

---
+++ Tests/TypingRealm.Messaging.Core.Tests/Data/MessageDataTests.cs
@@ -0,0 +1,13 @@
+﻿using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Tests;
+
+public class MessageDataTests : TestsBase
+{
+    [Fact]
+    public void ShouldBeSerializable()
+    {
+        AssertSerializable<MessageData>();
+    }
+}
