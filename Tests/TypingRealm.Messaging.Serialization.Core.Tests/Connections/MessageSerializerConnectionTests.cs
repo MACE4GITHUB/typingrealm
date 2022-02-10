842c659d15819ef9c29a09749061ddcbea1db3af
Ivan Zyranau <ewancoder@gmail.com>
2022-02-10 17:45:43 +0300

    Add first test for MessageSerializerConnection

---
+++ Tests/TypingRealm.Messaging.Serialization.Core.Tests/Connections/MessageSerializerConnectionTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoFixture;
+using Moq;
+using TypingRealm.Messaging.Serialization.Connections;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Serialization.Tests.Connections;
+
+public class MessageSerializerConnectionTests : TestsBase
+{
+    private readonly Mock<IConnection> _connection;
+    private readonly MessageSerializerConnection _sut;
+
+    public MessageSerializerConnectionTests()
+    {
+        _connection = Freeze<IConnection>();
+        _sut = Fixture.Create<MessageSerializerConnection>();
+    }
+
+    [Fact]
+    public async Task ShouldThrow_WhenReceivedMessageIsNotMessageData()
+    {
+        _connection.Setup(x => x.ReceiveAsync(Cts.Token))
+            .ReturnsAsync(Fixture.Create<object>());
+
+        await Assert.ThrowsAsync<InvalidOperationException>(
+            async () => await _sut.ReceiveAsync(Cts.Token));
+    }
+}
