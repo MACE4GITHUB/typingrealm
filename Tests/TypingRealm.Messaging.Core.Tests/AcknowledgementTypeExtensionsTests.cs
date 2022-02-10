dbef53dda0a0d2e1b669482b0d7fb3b07b4bf62b
Ivan Zyranau <ewancoder@gmail.com>
2022-02-10 13:31:48 +0300

    Add test for AcknowledgementTypeExtensions

---
+++ Tests/TypingRealm.Messaging.Core.Tests/AcknowledgementTypeExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace TypingRealm.Messaging.Tests;
+
+public class AcknowledgementTypeExtensionsTests
+{
+    [Fact]
+    public void ShouldNotRequireAcknowledgement_WhenAcknowledgementTypeIsNone()
+    {
+        var sut = AcknowledgementType.None;
+
+        Assert.False(sut.IsAcknowledgementRequired());
+    }
+
+    [Theory]
+    [InlineData(AcknowledgementType.Unspecified)]
+    [InlineData(AcknowledgementType.Received)]
+    [InlineData(AcknowledgementType.Handled)]
+    [InlineData((AcknowledgementType)99)]
+    public void ShouldRequireAcknowledgement_WhenAcknowledgementTypeIsNotNone(
+        AcknowledgementType sut)
+    {
+        Assert.True(sut.IsAcknowledgementRequired());
+    }
+}
