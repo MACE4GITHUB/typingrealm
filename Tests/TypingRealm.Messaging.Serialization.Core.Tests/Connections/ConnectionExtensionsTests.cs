d674f30b298aad9f687ac64c690cd364c3408dd8
Ivan Zyranau <ewancoder@gmail.com>
2022-02-11 16:16:39 +0300

    Add test for ConnectionExtensions

---
+++ Tests/TypingRealm.Messaging.Serialization.Core.Tests/Connections/ConnectionExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using TypingRealm.Messaging.Serialization.Connections;
+using TypingRealm.Testing;
+using Xunit;
+
+namespace TypingRealm.Messaging.Serialization.Tests.Connections;
+
+public class ConnectionExtensionsTests : TestsBase
+{
+    [Theory, AutoMoqData]
+    public void ShouldWrapConnectionInMessageSerializerConnection(
+        IConnection connection,
+        IMessageSerializer messageSerializer,
+        IMessageTypeCache messageTypeCache,
+        IMessageMetadataFactory messageMetadataFactory)
+    {
+        var sut = connection.AddCoreMessageSerialization(
+            messageSerializer, messageTypeCache, messageMetadataFactory);
+
+        Assert.IsType<MessageSerializerConnection>(sut);
+        Assert.Equal(connection, GetPrivateField(sut, "_connection"));
+        Assert.Equal(messageSerializer, GetPrivateField(sut, "_serializer"));
+        Assert.Equal(messageTypeCache, GetPrivateField(sut, "_messageTypeCache"));
+        Assert.Equal(messageMetadataFactory, GetPrivateField(sut, "_metadataFactory"));
+    }
+}
