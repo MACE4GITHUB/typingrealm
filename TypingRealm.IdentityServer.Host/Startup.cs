5d775fe976226a6d220e4b68fdfa49b6a5a21c8b
Ivan Zyranau <ewancoder@gmail.com>
2022-05-08 04:05:07 +0300

    Add CORS policies to IdentityServer host in Development environment

--- TypingRealm.IdentityServer.Host/Startup.cs
+++ TypingRealm.IdentityServer.Host/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace TypingRealm.IdentityServer.Host;
@@ -17,12 +19,50 @@ public class Startup
             .AddInMemoryIdentityResources(Config.IdentityResources)
             .AddInMemoryApiScopes(Config.ApiScopes)
             .AddInMemoryClients(Config.Clients);
+
+        if (DebugHelpers.IsDevelopment())
+        {
+            services.AddCors(options => options.AddPolicy(
+                "DevelopmentCorsPolicy",
+                builder => builder
+                    .WithOrigins(GetDevelopmentAllowedOrigins().ToArray())
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials()));
+        }
     }
 
     public void Configure(IApplicationBuilder app)
     {
         app.UseRouting();
+        app.UseCors("DevelopmentCorsPolicy");
         app.UseEndpoints(e => e.MapControllers());
         app.UseIdentityServer();
+    }
+
+    private static IEnumerable<string> GetDevelopmentAllowedOrigins()
+    {
+        var hosts = new[]
+        {
+                "127.0.0.1",
+                "localhost",
+                "typingrealm.com",
+                "dev.typingrealm.com"
+            };
+
+        var ports = new[]
+        {
+                "",
+                ":4200"
+            };
+
+        foreach (var host in hosts)
+        {
+            foreach (var port in ports)
+            {
+                yield return $"http://{host}{port}";
+                yield return $"https://{host}{port}";
+            }
+        }
     }
 }
