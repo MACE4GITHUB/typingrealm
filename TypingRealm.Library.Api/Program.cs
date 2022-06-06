268e846a61cad9e04d6de5066842ad1f707f510a
Ivan Zyranau <ewancoder@gmail.com>
2022-06-07 02:20:04 +0300

    Add basic reference-service comments to Library service
    This will be a reference Web API service covered with comments on how to
    use the framework.

--- TypingRealm.Library.Api/Program.cs
+++ TypingRealm.Library.Api/Program.cs
@@ -3,9 +3,19 @@ using TypingRealm.Hosting;
 using TypingRealm.Library.Api.Controllers;
 using TypingRealm.Library.Infrastructure;
 
+// The Library service serves as the reference WebAPI service of the whole
+// project, and as such is covered with meaningful comments.
+//
+// ApiController attribute can be applied once on the level of the assembly
+// instead of every controller class.
 [assembly: ApiController]
+
+// This creates a host that needs only API endpoints, registering all common
+// implementations shared between all project's Web API services.
 var builder = HostFactory.CreateWebApiApplicationBuilder(typeof(SentencesController).Assembly);
 
+// This Library.Infrastructure extension method should register everything
+// needed by the whole Library service.
 builder.Services.AddLibraryApi(builder.Configuration);
 
 await builder.Build().RunAsync();
