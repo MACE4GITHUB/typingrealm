ec93ee688f9692c3d068181c5ed0316923b1704a
Ivan Zyranau <ewancoder@gmail.com>
2022-06-12 02:40:09 +0300

    Add a TODO for future to think how to maintain validators closer to DTOs

--- TypingRealm.Library.Api/Books/Data/BookIdRouteParameter.cs
+++ TypingRealm.Library.Api/Books/Data/BookIdRouteParameter.cs
@@ -1,5 +1,7 @@
 ﻿namespace TypingRealm.Library.Api.Books.Data;
 
+// TODO: Consider declaring validators in the same file as DTO object.
+// But think how it can be done if we have SDK kind of project for DTOs.
 public sealed class BookIdRouteParameter
 {
     /// <include file='../ApiDocs.xml' path='Api/Library/Book/BookId/*'/>
