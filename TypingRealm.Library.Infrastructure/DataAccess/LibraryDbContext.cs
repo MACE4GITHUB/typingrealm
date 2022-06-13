a066c973dffd9cacff03405a46a66d3f3800233e
Ivan Zyranau <ewancoder@gmail.com>
2022-06-13 03:49:22 +0300

    Save CreatedAtUtc & UpdatedAtUtc fields

--- TypingRealm.Library.Infrastructure/DataAccess/LibraryDbContext.cs
+++ TypingRealm.Library.Infrastructure/DataAccess/LibraryDbContext.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using TypingRealm.Library.Infrastructure.DataAccess.Entities;
 
 namespace TypingRealm.Library.Infrastructure.DataAccess;
@@ -16,5 +20,25 @@ public sealed class LibraryDbContext : DbContext
     public DbSet<SentenceDao> Sentence { get; set; }
     public DbSet<WordDao> Word { get; set; }
     public DbSet<KeyPairDao> KeyPair { get; set; }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+    {
+        var now = DateTime.UtcNow;
+
+        var entities = ChangeTracker.Entries()
+            .Where(x => x.Entity is TrackableDao && (x.State == EntityState.Added || x.State == EntityState.Modified));
+
+        // TODO: Get current user context here and also save CreatedBy & UpdatedBy.
+        foreach (var entity in entities)
+        {
+            if (entity.State == EntityState.Added)
+            {
+                ((TrackableDao)entity.Entity).CreatedAtUtc = now;
+            }
+            ((TrackableDao)entity.Entity).UpdatedAtUtc = now;
+        }
+
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
 }
 #pragma warning restore CS8618
