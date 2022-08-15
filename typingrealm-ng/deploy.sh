f39814e2411d8452e398fbb6f33351ae62c05ece
Ivan Zyranau <ewancoder@gmail.com>
2022-08-15 20:27:44 +0300

    Add basic Caddy proxy

--- typingrealm-ng/deploy.sh
+++ typingrealm-ng/deploy.sh
@@ -2,9 +2,11 @@
 set -Eeuo pipefail
 
 if [ "$1" == "prod" ]; then
-    docker-compose -p tyr -f ./docker-compose.prod.yml up -d --build
+    docker-compose -p tyr -f ./docker-compose.production.yml up -d --build
 elif [ "$1" == "dev" ]; then
     docker-compose -p dev-tyr -f ./docker-compose.dev.yml up -d --build
 elif [ "$1" == "local" ]; then
     docker-compose -p local-tyr -f ./docker-compose.local.yml up -d --build
+elif [ "$1" == "infra" ]; then
+    docker-compose -p infra-tyr -f ./docker-compose.infra.yml up -d --build
 fi
