00c9b0f9ee3adfd01106a34ef8c4e151a7a1426b
Ivan Zyranau <ewancoder@gmail.com>
2022-08-15 14:54:39 +0300

    Basic Google Auth implementation, so far without our API tokens

---
+++ typingrealm-ng/frontend/auth.js
@@ -0,0 +1,54 @@
+const authAreaElement = document.getElementById('auth');
+
+export default class Auth {
+    #google;
+    #idToken = null;
+    #promiseResolves = [];
+
+    constructor(google, clientId) {
+        this.#google = google;
+        this.#google.initialize({
+            client_id: clientId,
+            callback: (auth) => this.#handleAuth(auth),
+            auto_select: true
+        });
+        this.#google.renderButton(
+            document.getElementById('authButton'),
+            { theme: 'outline', size: 'large' });
+        this.prompt();
+    }
+
+    getToken() {
+        if (this.#isExpiring()) {
+            authAreaElement.style.display = 'flex';
+            this.#google.prompt();
+
+            const promise = new Promise(resolve => {
+                this.#promiseResolves.push(resolve);
+            });
+
+            return promise;
+        }
+
+        return Promise.resolve(this.#idToken);
+    }
+
+    prompt() {
+        this.#google.prompt();
+    }
+
+    #handleAuth(auth) {
+        authAreaElement.style.display = 'none';
+
+        this.#idToken = auth.credential;
+
+        for (let resolve; resolve = this.#promiseResolves.pop(); ) {
+            resolve(this.#idToken);
+        }
+    }
+
+    #isExpiring() {
+        // TODO: Check expiration.
+        return !this.#idToken;
+    }
+}
--- typingrealm-ng/frontend/game.js
+++ typingrealm-ng/frontend/game.js
@@ -1,3 +1,8 @@
+import Auth from './auth.js';
 import listen from './listener.js';
 
-listen();
+window.onload = async function () {
+    const auth = new Auth(google.accounts.id, '400839590162-k6q520pk6lqs3vee6u18cdfhvtd07hf7');
+    await auth.getToken();
+    listen();
+}
