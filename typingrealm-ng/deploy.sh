952f2e21de255bc50e85daf8966361188b57c209
Ivan Zyranau <ewancoder@gmail.com>
2022-08-17 05:52:54 +0300

    Improve deployment scripts to be able to downscale

--- typingrealm-ng/deploy.sh
+++ typingrealm-ng/deploy.sh
@@ -2,16 +2,27 @@
 #set -Eeuo pipefail
 
 if [ "$1" == "prod" ]; then
-    docker-compose -p tyr -f ./docker-compose.production.yml up -d --build
+    if [ "$2" == "down" ]; then
+        docker-compose -p tyr -f ./docker-compose.production.yml down
+    else
+        docker-compose -p tyr -f ./docker-compose.production.yml up -d --build
+    fi
 elif [ "$1" == "dev" ]; then
-    docker-compose -p dev-tyr -f ./docker-compose.dev.yml up -d --build
+    if [ "$2" == "down" ]; then
+        docker-compose -p dev-tyr -f ./docker-compose.dev.yml down
+    else
+        docker-compose -p dev-tyr -f ./docker-compose.dev.yml up -d --build
+    fi
 elif [ "$1" == "local" ]; then
-    if [ "$2" == "stop" ]; then
-        docker-compose -p local-tyr -f ./docker-compose.local.yml stop
-        docker-compose -p local-tyr -f ./docker-compose.local.yml down --volumes
+    if [ "$2" == "down" ]; then
+        docker-compose -p local-tyr -f ./docker-compose.local.yml down
     else
         docker-compose -p local-tyr -f ./docker-compose.local.yml up -d --build
     fi
 elif [ "$1" == "infra" ]; then
-    docker-compose -p infra-tyr -f ./docker-compose.infra.yml up -d --build
+    if [ "$2" == "down" ]; then
+        docker-compose -p infra-tyr -f ./docker-compose.infra.yml down
+    else
+        docker-compose -p infra-tyr -f ./docker-compose.infra.yml up -d --build
+    fi
 fi
