8035912f268a083982e308f208af68174d950132
Ivan Zyranau <ewancoder@gmail.com>
2022-08-17 09:01:55 +0300

    Add an option to deploy all envs together

--- typingrealm-ng/deploy.sh
+++ typingrealm-ng/deploy.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 #set -Eeuo pipefail
 
-if [ "$1" == "prod" ]; then
+if [ "$1" == "all" ]; then
+    if [ "$2" == "down" ]; then
+        docker-compose -p local-tyr -f ./docker-compose.local.yml down
+        docker-compose -p dev-tyr -f ./docker-compose.dev.yml down
+        docker-compose -p tyr -f ./docker-compose.production.yml down
+    else
+        docker-compose -p tyr -f ./docker-compose.production.yml up -d --build
+        docker-compose -p dev-tyr -f ./docker-compose.dev.yml up -d --build
+        docker-compose -p local-tyr -f ./docker-compose.local.yml up -d --build
+    fi
+elif [ "$1" == "prod" ]; then
     if [ "$2" == "down" ]; then
         docker-compose -p tyr -f ./docker-compose.production.yml down
     else
