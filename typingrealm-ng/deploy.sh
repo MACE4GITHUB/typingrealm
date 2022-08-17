15517b6ec69f6e0853f4a4aad9507aa4b4f20d6d
Ivan Zyranau <ewancoder@gmail.com>
2022-08-17 03:14:44 +0300

    Add a script for stopping local containers

--- typingrealm-ng/deploy.sh
+++ typingrealm-ng/deploy.sh
@@ -6,7 +6,11 @@ if [ "$1" == "prod" ]; then
 elif [ "$1" == "dev" ]; then
     docker-compose -p dev-tyr -f ./docker-compose.dev.yml up -d --build
 elif [ "$1" == "local" ]; then
-    docker-compose -p local-tyr -f ./docker-compose.local.yml up -d --build
+    if [ "$2" == "stop" ]; then
+        docker-compose -p local-tyr -f ./docker-compose.local.yml stop
+    else
+        docker-compose -p local-tyr -f ./docker-compose.local.yml up -d --build
+    fi
 elif [ "$1" == "infra" ]; then
     docker-compose -p infra-tyr -f ./docker-compose.infra.yml up -d --build
 fi
