6c05864799e4508278db3ae9f8b1b2177d1907cf
Ivan Zyranau <ewancoder@gmail.com>
2022-08-17 15:23:25 +0300

    Generate text until there's at least 100 characters

--- typingrealm-ng/backend/typing/typing-node/app/texts/text-app.js
+++ typingrealm-ng/backend/typing/typing-node/app/texts/text-app.js
@@ -4,7 +4,12 @@ export default function(app) {
     const textGenerator = new TextGenerator();
 
     app.get('/api/texts', async (req, res) => {
-        const text = await textGenerator.generateText();
+        let text = '';
+        while (text.length < 100) {
+            text += await textGenerator.generateText() + ' ';
+        }
+        text = text.slice(0, -1);
+
         res.send({
             text: text
         });
