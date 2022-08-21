91526dde070491f36050f4e4618e710dc750b17c
Ivan Zyranau <ewancoder@gmail.com>
2022-08-21 12:58:37 +0300

    Add caching to .NET version of text retriever as well

---
+++ typingrealm-ng/backend/texts/dotnet/TypingRealm.Texts.Api/AheadOfTimeTextRetriever.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace TypingRealm.Texts.Api;
+
+public sealed class AheadOfTimeTextRetriever : ITextRetriever, IDisposable
+{
+    private const int MaxSize = 100;
+    private readonly ILogger<AheadOfTimeTextRetriever> _logger;
+    private readonly ITextRetriever _retriever;
+    private readonly ConcurrentQueue<string> _cache = new ConcurrentQueue<string>();
+    private readonly Timer _timer;
+
+    public AheadOfTimeTextRetriever(
+        ILogger<AheadOfTimeTextRetriever> logger,
+        ITextRetriever retriever)
+    {
+        _logger = logger;
+        _retriever = retriever;
+
+        // TODO: Figure out whether I can do async/await here.
+        _timer = new Timer(async e => await GetNextTextAsync(), null, TimeSpan.Zero, TimeSpan.FromSeconds(2));
+    }
+
+    public ValueTask<string> RetrieveTextAsync()
+    {
+        if (_cache.TryDequeue(out var value))
+            return new(value);
+
+        return _retriever.RetrieveTextAsync();
+    }
+
+    private async ValueTask GetNextTextAsync()
+    {
+        if (_cache.Count >= MaxSize)
+            return;
+
+        _cache.Enqueue(await _retriever.RetrieveTextAsync());
+        _logger.LogInformation("Got next text.");
+    }
+
+    public void Dispose()
+    {
+        // TODO: Implement Disposable using my primitives helpers.
+        _timer.Dispose();
+    }
+}
--- typingrealm-ng/backend/texts/dotnet/TypingRealm.Texts.Api/Program.cs
+++ typingrealm-ng/backend/texts/dotnet/TypingRealm.Texts.Api/Program.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using TypingRealm.Texts.Api;
 using TypingRealm.Texts.Api.Controllers;
 
@@ -12,10 +14,15 @@ builder.Services.AddControllers();
 
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<Counter>();
-builder.Services.AddTransient<ITextRetriever, TextRetriever>();
+builder.Services.AddSingleton<ITextRetriever>(
+    c => new AheadOfTimeTextRetriever(
+        c.GetRequiredService<ILogger<AheadOfTimeTextRetriever>>(),
+        new TextRetriever(c.GetRequiredService<IHttpClientFactory>())));
 
 var app = builder.Build();
 app.UseRouting();
 app.UseEndpoints(endpoints => endpoints.MapControllers());
 
+// Start retrieving texts.
+app.Services.GetRequiredService<ITextRetriever>();
 await app.RunAsync();
