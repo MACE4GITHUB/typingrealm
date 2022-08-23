3d2dd93dc6e5a4218700a1ad9f199a0c29987560
Ivan Zyranau <ewancoder@gmail.com>
2022-08-23 21:45:58 +0300

    Fix website not working if you haven't typed a single text

--- typingrealm-ng/frontend/app/listener.js
+++ typingrealm-ng/frontend/app/listener.js
@@ -68,12 +68,16 @@ function showStatistics(statisticsResult) {
     }
 
     statisticsElement.innerHTML += '<div class="statistics-entry" style="margin-top: 50px">Press ENTER to start typing next text</div>';
-    statisticsElement.innerHTML += '<div class="statistics-entry" style="margin-bottom: 50px; font-size: 1.5rem">Below is YOUR all-time statistics</div>';
-    addAnalyticsEntry('Total characters typed', statisticsResult.allTime.totalCharactersCount);
-    addAnalyticsEntry('Total errors', statisticsResult.allTime.errorCharactersCount);
-    addAnalyticsEntry('Total time taken', `${humanizeDuration(Math.round(statisticsResult.allTime.totalTimeMs / 100) * 100)}`);
-    addAnalyticsEntry('Average speed', `${statisticsResult.allTime.speedWpm.toFixed(2)} WPM`);
-    addAnalyticsEntry('Precision', `${statisticsResult.allTime.precision.toFixed(2)} %`);
+
+    // TODO: Also don't show all-users statistics if there is none at all.
+    if (statisticsResult.allTime.speedWpm) {
+        statisticsElement.innerHTML += '<div class="statistics-entry" style="margin-bottom: 50px; font-size: 1.5rem">Below is YOUR all-time statistics</div>';
+        addAnalyticsEntry('Total characters typed', statisticsResult.allTime.totalCharactersCount);
+        addAnalyticsEntry('Total errors', statisticsResult.allTime.errorCharactersCount);
+        addAnalyticsEntry('Total time taken', `${humanizeDuration(Math.round(statisticsResult.allTime.totalTimeMs / 100) * 100)}`);
+        addAnalyticsEntry('Average speed', `${statisticsResult.allTime.speedWpm.toFixed(2)} WPM`);
+        addAnalyticsEntry('Precision', `${statisticsResult.allTime.precision.toFixed(2)} %`);
+    }
     statisticsElement.innerHTML += '<div class="statistics-entry" style="margin: 100px; font-size: 1.5rem">Below is the global statistics for ALL users</div>';
     addAnalyticsEntry('Total characters typed', statisticsResult.global.totalCharactersCount);
     addAnalyticsEntry('Total errors', statisticsResult.global.errorCharactersCount);
