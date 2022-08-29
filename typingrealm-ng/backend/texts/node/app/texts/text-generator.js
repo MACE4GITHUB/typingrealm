5bd2305b06103cd9b11b5dbd268f99fe45d4b5d3
Ivan Zyranau <ewancoder@gmail.com>
2022-08-29 05:32:50 +0300

    Add unit tests for TextGenerator

--- typingrealm-ng/backend/texts/node/app/texts/text-generator.js
+++ typingrealm-ng/backend/texts/node/app/texts/text-generator.js
@@ -1,10 +1,21 @@
-import fetch from 'node-fetch';
+import nodeFetch from 'node-fetch';
 
 export default class TextGenerator {
-    // eslint-disable-next-line class-methods-use-this
+    #fetch;
+
+    constructor(fetch = nodeFetch) {
+        this.#fetch = fetch;
+    }
+
     async generateText() {
-        const response = await fetch('https://api.quotable.io/random');
+        const response = await this.#fetch('https://api.quotable.io/random');
         const json = await response.json();
+
+        if (!json.content) {
+            throw new Error('Content is missing.');
+        }
+
+        console.log(json);
         return json.content;
     }
 }
--- typingrealm-ng/backend/texts/node/app/texts/text-generator.test.js
+++ typingrealm-ng/backend/texts/node/app/texts/text-generator.test.js
@@ -1,11 +1,44 @@
 import TextGenerator from './text-generator.js';
 
-describe('TextGenerator', () => {
-    test('Should get non-empty text', async () => {
-        const sut = new TextGenerator();
-        const text = await sut.generateText();
-        console.log(text);
+function fetchMockFactory(data, validUrl) {
+    return function fetchMock(url) {
+        if (url === validUrl) {
+            return Promise.resolve({
+                json: () => Promise.resolve(data)
+            });
+        }
 
-        expect(text.length >= 3).toBeTruthy();
+        return Promise.reject();
+    };
+}
+
+describe('TextGenerator', () => {
+    test('should get proper content', async () => {
+        const sut = new TextGenerator(fetchMockFactory({
+            content: 'content'
+        }, 'https://api.quotable.io/random'));
+        const text = await sut.generateText();
+
+        expect(text).toBe('content');
+    });
+
+    describe('should throw', () => {
+        test('when API content is missing', async () => {
+            const sut = new TextGenerator(fetchMockFactory({
+                someOtherProperty: 'content'
+            }, 'https://api.quotable.io/random'));
+            await expect(sut.generateText())
+                .rejects
+                .toThrow();
+        });
+
+        test('when API content is empty', async () => {
+            const sut = new TextGenerator(fetchMockFactory({
+                content: ''
+            }, 'https://api.quotable.io/random'));
+            await expect(sut.generateText())
+                .rejects
+                .toThrow();
+        });
     });
 });
